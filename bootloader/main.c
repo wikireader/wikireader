dc1df4496571b5c015f3832e134c2feccc02dab4
zonque <zonque@974c56cd-8151-0410-98c4-7bb2f43fa8c1>
2008-07-22 13:36:45 +0000

    register set cleanups and more rs232 bootloader code
    git-svn-id: https://wikipediardware.googlecode.com/svn/trunk@6 974c56cd-8151-0410-98c4-7bb2f43fa8c1

--- bootloader/main.c
+++ bootloader/main.c
@@ -9,7 +9,9 @@ int main(void) {
 	unsigned char b;
 
 	init_pins();
-	
+
+	SDCARD_CS_HI();
+
 	/* serial line 0: 8-bit async, no parity, internal clock, 1 stop bit */
 	REG_EFSIF0_CTL = 0xc3;
 
@@ -17,14 +19,19 @@ int main(void) {
 	REG_SPI_CTL1 = 0x0001c73;
 
 	/* EEPROM: write enable */
+	EEPROM_CS_LO();
 	REG_SPI_TXD = 0x06;
 	WAIT_FOR_SPI_RDY();
+	EEPROM_CS_HI();
 
 	/* EEPROM: chip erase */
+	EEPROM_CS_LO();
 	REG_SPI_TXD = 0x60;
 	WAIT_FOR_SPI_RDY();
+	EEPROM_CS_HI();
 
 	/* EEPROM: auto address increment (AAI), start at address 0 */
+	EEPROM_CS_LO();
 	REG_SPI_TXD = 0xaf;
 	WAIT_FOR_SPI_RDY();
 	
@@ -37,16 +44,26 @@ int main(void) {
 	REG_SPI_TXD = 0x0;
 	WAIT_FOR_SPI_RDY();
 
+	do {} while (!(REG_EFSIF0_STATUS & 0x1));
+	b = REG_EFSIF0_RXD;
+
+	REG_SPI_TXD = b;
+	WAIT_FOR_SPI_RDY();
+	EEPROM_CS_HI();
+
 	/* get more bytes and write them to the EEPROM */
 	for (;;) {
-		while (!(REG_EFSIF0_STATUS & 0x1));
+		do {} while (!(REG_EFSIF0_STATUS & 0x1));
 		b = REG_EFSIF0_RXD;
 
+		EEPROM_CS_LO();
+		
 		REG_SPI_TXD = b;
 		WAIT_FOR_SPI_RDY();
-
+	
 		REG_SPI_TXD = 0xaf;
 		WAIT_FOR_SPI_RDY();
+		EEPROM_CS_HI();
 	}
 
 	return 0;
--- bootloader/wikireader.h
+++ bootloader/wikireader.h
@@ -5,6 +5,9 @@
 {					\
 	/* P13 & P14: debug LEDs */	\
 	REG_P1_IOC1 = 0x18;		\
+					\
+	/* P50 & P52: CS lines */	\
+	REG_P5_IOC5 = 0x05;		\
 }
 
 #define DEBUGLED1_ON()	REG_P1_P1D &= ~(1 << 4)
@@ -12,6 +15,12 @@
 
 #define DEBUGLED2_ON()	REG_P1_P1D &= ~(1 << 3)
 #define DEBUGLED2_OFF()	REG_P1_P1D |=  (1 << 3)
+
+#define SDCARD_CS_LO()	REG_P5_P5D &= ~(1 << 0);
+#define SDCARD_CS_HI()	REG_P5_P5D |=  (1 << 0);
+
+#define EEPROM_CS_LO()	REG_P5_P5D &= ~(1 << 2);
+#define EEPROM_CS_HI()	REG_P5_P5D |=  (1 << 2);
 
 #endif /* WIKIREADER_H */
 
