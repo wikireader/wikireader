03d155bea8a022c0ee0b6dbe1300437b95cb1f1d
zonque <zonque@974c56cd-8151-0410-98c4-7bb2f43fa8c1>
2008-08-13 11:32:18 +0000

    GPL3 disclaimers added
    git-svn-id: https://wikipediardware.googlecode.com/svn/trunk@29 974c56cd-8151-0410-98c4-7bb2f43fa8c1

--- bootloader/e07load/main.c
+++ bootloader/e07load/main.c
@@ -1,3 +1,21 @@
+/*
+    e07 bootloader suite
+    Copyright (c) 2008 Daniel Mack <daniel@caiaq.de>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -32,8 +50,11 @@ static void usage(const char *argv0)
 	msg("\n");
 	msg("Lines starting with '#' are ignored, addresses may be given in decimal,\n");
 	msg("octal or hexadecimal format. Filenames may be relative to current pwd.\n");
-
+	
 	msg("\n");
+	msg("This program comes with ABSOLUTELY NO WARRANTY.\n");
+	msg("This is free software, and you are welcome to redistribute it\n");
+	msg("under certain conditions; see file LICENSE.txt for details.\n");
 	exit(1);
 }
 
