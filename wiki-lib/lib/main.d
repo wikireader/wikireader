7ad53083129117670100837a66f8d71c93531f7d
zecke23 <zecke23@974c56cd-8151-0410-98c4-7bb2f43fa8c1>
2008-09-24 16:30:43 +0000

    [wiki-lib] Remove autogenerated file
    git-svn-id: https://wikipediardware.googlecode.com/svn/trunk@109 974c56cd-8151-0410-98c4-7bb2f43fa8c1

--- wiki-lib/lib/main.d
+++
@@ -1,4 +0,0 @@
-main.o: lib/main.c /usr/include/string.h /usr/include/_types.h \
-  /usr/include/sys/_types.h /usr/include/sys/cdefs.h \
-  /usr/include/machine/_types.h /usr/include/i386/_types.h \
-  include/external_interface.h
