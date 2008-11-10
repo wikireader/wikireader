eb39c46b1fe585157a4dae3f3648e429be589780
zecke23@gmail.com <zecke23@gmail.com@974c56cd-8151-0410-98c4-7bb2f43fa8c1>
2008-11-10 23:42:36 +0000

    [wiki-tools] Add a test case to parse and handle redirects
        #REDIRECT is a way for an article to link to another. For our
        usage we only want to handle the proper articles and all redirects
        should be handled in the index already.
        E.g. the index should point to the right article for the different
        alternatives.
    
    git-svn-id: https://wikipediardware.googlecode.com/svn/trunk@205 974c56cd-8151-0410-98c4-7bb2f43fa8c1

---
+++ wiki-tools/tests/tests.pro
@@ -0,0 +1,21 @@
+TEMPLATE = app
+TARGET = tst_wiki-tools
+SOURCES += tst_wikitools.cc
+
+INCLUDEPATH += ../src ../mozilla-sha1
+DEPENDPATH += ../src
+SOURCES += \
+    ../src/Article.cc \
+    ../src/ArticleHandler.cc \
+    ../src/CreateIndex.cc \
+    ../src/CreateText.cc \
+    ../src/ExtractText.cc \
+    ../src/ExtractTextHashed.cc \
+    ../src/ExtractTitles.cc \
+    ../src/ExtractWords.cc \
+    ../src/SplitArticles.cc \
+    ../src/StreamReader.cc \
+    ../src/Title.cc \
+    ../mozilla-sha1/sha1.c
+
+QT += testlib
---
+++ wiki-tools/tests/tst_wikitools.cc
@@ -0,0 +1,70 @@
+/*
+ * Wiki Handling Tool - Tests
+ *
+ * Copyright (C) 2008 Openmoko Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <QtTest/QtTest>
+#include "Article.h"
+
+class ArticleRedirectTest : public QObject {
+    Q_OBJECT
+public:
+    ArticleRedirectTest();
+
+private Q_SLOTS:
+    void testRedirect();
+    void testRedirect_data();
+};
+
+
+ArticleRedirectTest::ArticleRedirectTest()
+{}
+
+
+void ArticleRedirectTest::testRedirect_data()
+{
+    QTest::addColumn<QString>("article");
+    QTest::addColumn<bool>("isRedirect");
+    QTest::addColumn<QString>("target");
+
+    QTest::newRow("redirect 1")
+            << QString::fromLatin1("#REDIRECT [[Geography of Afghanistan]] {{R from CamelCase}}")
+            << true
+            << QString::fromLatin1("Geography of Afghanistan");
+    QTest::newRow("redirect 2")
+            << QString::fromLatin1("#REDIRECT [[African American]] {{R from CamelCase}}")
+            << true
+            << QString::fromLatin1("African American");
+    QTest::newRow("normal 1")
+            << QString::fromLatin1("{{Otherusesof|A}}")
+            << false
+            << QString();
+}
+
+void ArticleRedirectTest::testRedirect()
+{
+    QFETCH(QString, article);
+    QFETCH(bool, isRedirect);
+    QFETCH(QString, target);
+
+    Article wikiArticle(Title(), article);
+    QCOMPARE(wikiArticle.isRedirect(), isRedirect);
+    QCOMPARE(wikiArticle.redirectsTo(), target);
+}
+
+QTEST_MAIN(ArticleRedirectTest)
+#include "tst_wikitools.moc"
