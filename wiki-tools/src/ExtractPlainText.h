42ecd7494bf3d9f0f2e5530fd45209e3db9fcb28
zecke23@gmail.com <zecke23@gmail.com@974c56cd-8151-0410-98c4-7bb2f43fa8c1>
2008-11-13 18:06:08 +0000

    [wiki-tools] Add passes to extract html and plain text
    git-svn-id: https://wikipediardware.googlecode.com/svn/trunk@225 974c56cd-8151-0410-98c4-7bb2f43fa8c1

---
+++ wiki-tools/src/ExtractPlainText.h
@@ -0,0 +1,35 @@
+/*
+ * Wiki Handling Tool
+ *
+ * Copyright (C) 2008 Openmoko Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ExtractPlainText_h
+#define ExtractPlainText_h
+
+#include "ArticleHandler.h"
+
+/**
+ * Extract the "Text" of wikipedia into one big file
+ */
+class ExtractPlainText : public FileOutputArticleHandler {
+public:
+    ExtractPlainText(const QString& outputFile);
+
+    void handleArticle(const Article& text);
+};
+
+#endif
