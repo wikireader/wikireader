051f231518bc67b4e6b63e7b8bc962b8d13fb0ed
zonque <zonque@974c56cd-8151-0410-98c4-7bb2f43fa8c1>
2008-12-09 21:09:18 +0000

    - write the index table as first part of the output file - some more cleanups
    git-svn-id: https://wikipediardware.googlecode.com/svn/trunk@312 974c56cd-8151-0410-98c4-7bb2f43fa8c1

--- glyphs/mk_glyphfile/image.c
+++ glyphs/mk_glyphfile/image.c
@@ -31,6 +31,8 @@ static int bpp = 4;
 static int output_bitpos = 0;
 static unsigned char output_char = 0;
 static int output_fd = -1;
+static int output_pos = 0;
+static unsigned int index_table[0xffff] = { 0 };
 
 static int open_output_file(const char *filename)
 {
@@ -46,18 +48,23 @@ static int open_output_file(const char *filename)
 	return 0;
 }
 
-static void output_pixel(unsigned int val)
+static int output_pixel(unsigned int val)
 {
 	val >>= (8 - bpp);
 	output_char |= val;
 	output_bitpos += bpp;
 
 	if (output_bitpos >= 8) {
-		write(output_fd, &output_char, 1);
+		if (write(output_fd, &output_char, 1) != 1)
+			return -3;
+		
+		output_pos++;
 		output_bitpos = 0;
 		output_char = 0;
 	} else
 		output_char <<= bpp;
+
+	return 0;
 }
 
 static int image_read_file(const char *filename, int unicode)
@@ -65,7 +72,9 @@ static int image_read_file(const char *filename, int unicode)
 	FILE *in;
 	gdImagePtr im;
 	int x, y, w, h;
-	
+
+	unicode &= 0xffff;
+
 	in = fopen(filename, "r");
 	if (!in) {
 		printf("Unable to open input file: %s\n", strerror(errno));
@@ -77,27 +86,34 @@ static int image_read_file(const char *filename, int unicode)
 
 	if (!im) {
 		printf("Unable to create gdImage from file.\n");
-		return -1;
+		return -2;
 	}
 
+	index_table[unicode] = output_pos;
 	w = gdImageSX(im);
 	h = gdImageSY(im);
+	printf("adding glyph for unicode #%d from %s (%d x %d pixels), offset %d\n",
+		unicode, filename, w, h, output_pos);
 
-	write(output_fd, &unicode, 2);
-	write(output_fd, &w, 1);
-	write(output_fd, &h, 1);
+	if (write(output_fd, &w, 1) != 1)
+		return -3;
+	
+	if (write(output_fd, &h, 1) != 1)
+		return -3;
+
+	output_pos += 2;
 	output_bitpos = output_char = 0;
 
 	for (y = 0; y < h; y++)
 		for (x = 0; x < w; x++) {
 			int c = gdImageGetPixel(im, x, y);
 			int p = gdImageRed(im, c);
-			output_pixel(255 - p);
+			int ret = output_pixel(255 - p);
+			if (ret)
+				return ret;
 		}
 
 	gdImageDestroy(im);
-	printf("added glyph for unicode #%d from %s (%d x %d pixels)\n",
-		unicode, filename, w, h);
 	return 0;
 }
 
@@ -131,9 +147,16 @@ int main (int argc, char **argv)
 	if (open_output_file(argv[2]) < 0)
 		return errno;
 
+	if (write(output_fd, index_table, sizeof(index_table)) != sizeof(index_table)) {
+		printf("WARNING: unable to write: %s\n", strerror(errno));
+		return errno;
+	}
+
+	output_pos += sizeof(index_table);
+
 	while (fgets(buf, sizeof(buf), mapfile)) {
 		char *s, *filename;
-		int unicode;
+		int ret, unicode;
 
 		if (strlen(buf) == 0 || buf[0] == '#')
 			continue;
@@ -151,8 +174,19 @@ int main (int argc, char **argv)
 			continue;
 
 		strchomp(filename);
-		image_read_file(filename, unicode);
+		ret = image_read_file(filename, unicode);
+		if (ret)
+			printf("WARNING: image_read_file() returned %d\n", ret);
 	}
+
+	lseek(output_fd, 0, SEEK_SET);
+	if (write(output_fd, index_table, sizeof(index_table)) != sizeof(index_table))
+		printf("WARNING: unable to write: %s\n", strerror(errno));
+
+	int i;
+	for (i = 0; i < sizeof(index_table)/4; i++)
+		if (index_table[i] != 0)
+			printf(" %d: %d\n", i, index_table[i]);
 
 	close(output_fd);
 	return 0;
