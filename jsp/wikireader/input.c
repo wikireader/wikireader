81c895b55c51ca559b8b73a53d8384c653912241
zonque <zonque@974c56cd-8151-0410-98c4-7bb2f43fa8c1>
2008-12-11 13:08:48 +0000

    - only allow 8bit input and key press (no key release) from Topper's console - added wikilib.o to Makefile
    git-svn-id: https://wikipediardware.googlecode.com/svn/trunk@327 974c56cd-8151-0410-98c4-7bb2f43fa8c1

--- jsp/wikireader/input.c
+++ jsp/wikireader/input.c
@@ -11,6 +11,13 @@ int wl_input_wait(struct wl_input_event *ev)
 {
 	ev->type = WL_INPUT_EV_TYPE_KEYBOARD;
 	syscall(serial_rea_dat(TASK_PORTID, &ev->val_a, 1));
+
+	/* force the result to 8bit */
+	ev->val_a &= 0xff;
+
+	/* key press only from console, no key release */
+	ev->val_b = 1;
+	
 	return 0;
 }
 
