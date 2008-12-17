13212362431ab625e2064be548eb7d63860f18b9
xiangfu.z <xiangfu.z@974c56cd-8151-0410-98c4-7bb2f43fa8c1>
2008-12-17 13:54:24 +0000

    add detece-wikireader program
    git-svn-id: https://wikipediardware.googlecode.com/svn/trunk@380 974c56cd-8151-0410-98c4-7bb2f43fa8c1

---
+++ detect-wikireader/src/check-wikireader.c
@@ -0,0 +1,36 @@
+/*
+ * (C) Copyright 2008 OpenMoko, Inc.
+ * Author: xiangfu liu <xiangfu@openmoko.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <unistd.h>
+#include <syslog.h>
+
+extern char **environ;
+
+int check_wikireader(char *path)
+{
+	if (access(path, R_OK) == 0 ) {
+		syslog(LOG_INFO, "yes. this is wikireader sd card --%s--.", path );
+		/*TODO: open websit wiki.openmoko.com */
+		system("firefox   http://wiki.openmoko.org");
+		syslog(LOG_INFO, "firefox");
+	} else {
+		system("firefox   http://wiki.openmoko.org");
+		syslog(LOG_INFO, "no. this is not wikireader sd card --%s--.", path );
+	}
+
+	return 0;
+}
---
+++ detect-wikireader/src/main.c
@@ -0,0 +1,161 @@
+/*
+ * (C) Copyright 2008 OpenMoko, Inc.
+ * Author: xiangfu liu <xiangfu@openmoko.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <syslog.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include "wiki-inotify.h"
+
+#define RUNNING_DIR "/"
+#define LOCK_FILE "/home/xiangfu/.detect-wikireader.lock"
+#define LOG_FILE "/home/xiangfu/detect-wikireader.log" 
+
+int log_message(char *filename, char*message)
+{
+	FILE *logfile;
+	logfile=fopen(filename,"a");
+	if (!logfile) 
+		return -1;
+	fprintf(logfile,"%s\n",message);
+	fclose(logfile);
+	return 0;
+} 
+
+void signal_handler(int signo)
+{
+	switch(signo) {
+	case SIGHUP:
+		signal(SIGHUP,signal_handler);
+		syslog(LOG_INFO,"hangup signal catched");
+		break;
+	case SIGTERM:
+		/* catched signal sent by kill(1) command */ 
+		syslog(LOG_INFO, "terminate signal catched");
+		closelog(); 
+		close_monitor();
+		exit(EXIT_SUCCESS); 
+		break;
+	default:
+		break;
+	}
+}
+
+int daemon_init(void) 
+{
+	int lfp;
+	char str[10];
+
+	if (getppid() == 1) 
+		return -1; /* already a daemon */
+
+        /* Our process ID and Session ID */
+        pid_t pid, sid;
+        
+        /* Fork off the parent process */
+        pid = fork();
+        if (pid < 0) {
+                exit(EXIT_FAILURE);
+        }
+        /* If we got a good PID, then
+           we can exit the parent process. */
+        if (pid > 0) {
+                exit(EXIT_SUCCESS);
+        }
+
+        /* Change the file mode mask */
+        umask(0);
+                
+        /* Open any logs here */        
+                
+        /* Create a new SID for the child process */
+        sid = setsid();
+        if (sid < 0) {
+                /* Log the failure */
+                exit(EXIT_FAILURE);
+        }
+        
+        /* Change the current working directory */
+        if ((chdir(RUNNING_DIR)) < 0) {
+                /* Log the failure */
+                exit(EXIT_FAILURE);
+        }
+        
+	lfp = open(LOCK_FILE, O_RDWR|O_CREAT, 0640);
+	if (lfp<0) {
+		syslog(LOG_INFO, "can not open lock file."); 
+		exit(EXIT_FAILURE);
+	}
+	if (lockf(lfp,F_TLOCK,0) < 0) {
+		syslog(LOG_INFO, "can not lock."); 
+		exit(EXIT_FAILURE); 
+	}
+
+	sprintf(str,"%d\n",getpid());	/* first instance continues */
+	write(lfp, str, strlen(str)); /* record pid to lockfile */
+
+	/* close all descriptors */
+	int desc;
+	for (desc = getdtablesize(); desc >= 0; --desc) 
+		close(desc); 
+
+	return 0;
+} 
+
+
+int main(void) {
+
+	if (daemon_init() != 0) { 
+		printf("can't fork self\n"); 
+		exit(EXIT_FAILURE); 
+	} 
+
+	openlog("openmoko-detect-wikireader", LOG_PID, LOG_USER); 
+	syslog(LOG_INFO, "program started."); 
+
+	/* arrange to catch the signal */ 
+	signal(SIGCHLD,SIG_IGN); /* ignore child */
+	signal(SIGTSTP,SIG_IGN); /* ignore tty signals */
+	signal(SIGTTOU,SIG_IGN);
+	signal(SIGTTIN,SIG_IGN);
+	signal(SIGHUP,signal_handler); /* catch hangup signal */
+	signal(SIGTERM,signal_handler); /* catch kill signal */
+        
+        /* Daemon-specific initialization goes here */
+	int rt;
+	if ((rt = init_monitor()) != 0) {
+		syslog(LOG_INFO, "init_monitor error: %d", rt); 
+		exit(EXIT_SUCCESS);
+	}
+
+        /* The Big Loop */
+        while (1) {
+		/* Do some task here ... */
+		run_monitor();           
+		sleep(1); /* wait 30 seconds */
+        }
+
+	exit(EXIT_SUCCESS);
+
+}
