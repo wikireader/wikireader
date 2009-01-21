d1e839b76e9f16f515af9186719ee657cf331935
zecke23@gmail.com <zecke23@gmail.com@974c56cd-8151-0410-98c4-7bb2f43fa8c1>
2009-01-21 04:33:38 +0000

    Add create_index.sh. It comes without documentation... but that is a feature
    git-svn-id: https://wikipediardware.googlecode.com/svn/trunk@496 974c56cd-8151-0410-98c4-7bb2f43fa8c1

---
+++ indexing/create_index.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "Input file is $1"
+cat $1 | LC_ALL=C sort -u > indexfile.index.sorted
+./locate/lbigram < indexfile.index.sorted | sort -nr | perl -ne '/^\s*[0-9]+\s(..)$/ && print $1' > indexfile.index.bigrams
+./locate/lcode indexfile.index.bigrams < indexfile.index.sorted > index.locate.db
+./locate/lsearcher -f index.locate.db -c index.locate.prefix -n
