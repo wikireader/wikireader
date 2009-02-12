d1356f6568fbfb6e69e82485c50e276ab18f1ef3
zecke23 <zecke23@974c56cd-8151-0410-98c4-7bb2f43fa8c1>
2009-02-12 18:30:04 +0000

    [guilib] Implement interactive search....
    Handle key events to do the searching. Backspace
    is still borked and some other things do not work...
    e.g. if you type abcdefg you will end up with an empty
    search becuase the best match was already found...
    
    git-svn-id: https://wikipediardware.googlecode.com/svn/trunk@772 974c56cd-8151-0410-98c4-7bb2f43fa8c1

--- wiki-lib/generic/wikilib.c
+++ wiki-lib/generic/wikilib.c
@@ -14,8 +14,7 @@ int wikilib_init (void)
 int wikilib_run(void)
 {
 	void *a;
-	int fd, i, j;
-	char *result;
+	int fd, i;
 
 	a = malloc(512);
 	msg(MSG_INFO, " a = %p\n", a);
@@ -42,36 +41,35 @@ int wikilib_run(void)
 	 * test searching code...
 	 */
 	search_init();
-	search_reset();
-
-	char *search_string = "india";
-	for (i = 0; i < (int)strlen(search_string); ++i) {
-		msg(MSG_INFO, "Adding to search : '%c'\n", search_string[i]);
-		search_add(search_string[i]);
-		j = 0;
-		while (j++ < 5 && (result = search_fetch_result()))
-			msg(MSG_INFO, "Result: %s\n", result);
-		search_print_stats();
-	}
-
-
-	/*
-	 * test rendering of strings... it is asusmed that
-	 * font '6' has the necessary glaphs
-	 */
-	render_string(6, "0123456789 ", 10, 10);
-	render_string(6, "abcdefghijklmnopqrstuvwxyz ", 10, 25);
-	render_string(6, "ABCDEFGHIJKLMNOPQSRTUVWXYZ ", 10, 40);
-	render_string(6, ".,;", 10, 55);
 
 	for (;;) {
 		struct wl_input_event ev;
 		wl_input_wait(&ev);
 
 		switch (ev.type) {
-		case WL_INPUT_EV_TYPE_KEYBOARD:
-			msg(MSG_INFO, "%s() got key: %d\n", __func__, ev.key_event.keycode);
+		case WL_INPUT_EV_TYPE_KEYBOARD: {
+			char *result;
+			int j = 0;
+
+			if (ev.key_event.keycode == 8) {
+				search_remove_char();
+			} else if (isalnum(ev.key_event.keycode) ||
+				    isspace(ev.key_event.keycode)) {
+				msg(MSG_INFO, "Adding to search : '%c'\n",
+					ev.key_event.keycode);
+				search_add(ev.key_event.keycode);
+			} else {
+				msg(MSG_INFO, "%s() unhandled key: %d\n", __func__,
+					ev.key_event.keycode);
+				continue;
+			}
+
+			while (j++ < 5 && (result = search_fetch_result()))
+				msg(MSG_INFO, "Result: %s\n", result);
+			search_print_stats();
+
 			break;
+		}
 		case WL_INPUT_EV_TYPE_TOUCH:
 			msg(MSG_INFO, "%s() touch event @%d,%d val %d\n", __func__,
 				ev.touch_event.x,
