5c8e201fc02fc3d5d02dd5722cf34b504950abe5
john.cylee <john.cylee@974c56cd-8151-0410-98c4-7bb2f43fa8c1>
2009-02-18 03:52:00 +0000

    Fix monochrome LCD_VRAM_SIZE
    git-svn-id: https://wikipediardware.googlecode.com/svn/trunk@830 974c56cd-8151-0410-98c4-7bb2f43fa8c1

--- common/lcd.h
+++ common/lcd.h
@@ -7,11 +7,10 @@ void init_lcd(void);
 
 #if LCD_MONOCHROME
 #define LCD_VRAM	(0x80000)
-#define LCD_VRAM_SIZE	(0x2FFF)
+#define LCD_VRAM_SIZE	(0x2580)
 #else
 #define LCD_VRAM	(0x10100000)
 #define LCD_VRAM_SIZE	((320 * 240) / 2)
 #endif
 
 #endif /* LCD_H */
-
