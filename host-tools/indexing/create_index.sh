3d91fcf022d799d02acc1b9e116752e7630ac138
zecke23 <zecke23@974c56cd-8151-0410-98c4-7bb2f43fa8c1>
2009-02-26 03:18:31 +0000

    [indexing] The device attempts to use pedia.idx and pedia.jmp.. rename
    Update the documentation, hinted by Xiangfu.
    
    git-svn-id: https://wikipediardware.googlecode.com/svn/trunk@868 974c56cd-8151-0410-98c4-7bb2f43fa8c1

--- host-tools/indexing/create_index.sh
+++ host-tools/indexing/create_index.sh
@@ -3,5 +3,5 @@
 echo "Input file is $1"
 cat $1 | LC_ALL=C sort -u | ./sort.py > indexfile.index.sorted
 ./locate/lbigram < indexfile.index.sorted | sort -nr | perl -ne '/^\s*[0-9]+\s(..)$/ && print $1' > indexfile.index.bigrams
-./locate/lcode indexfile.index.bigrams < indexfile.index.sorted > index.locate.db
-./locate/lsearcher -f index.locate.db -c index.locate.prefix -n
+./locate/lcode indexfile.index.bigrams < indexfile.index.sorted > pedia.idx
+./locate/lsearcher -f pedia.idx -c pedia.jmp -n
