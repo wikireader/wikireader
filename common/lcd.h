134d036e0c04d4aadbf7b0cbff1962a6e0f512c1
john.cylee <john.cylee@974c56cd-8151-0410-98c4-7bb2f43fa8c1>
2009-03-02 09:54:20 +0000

    Define device specific LCD_WIDTH and LCD_HEIGHT
    git-svn-id: https://wikipediardware.googlecode.com/svn/trunk@878 974c56cd-8151-0410-98c4-7bb2f43fa8c1

--- common/lcd.h
+++ common/lcd.h
@@ -5,12 +5,20 @@
 
 void init_lcd(void);
 
+#ifdef BOARD_S1C33E07
+#define LCD_WIDTH	(320)
+#define LCD_HEIGHT	(240)
+#else
+#define LCD_WIDTH	(240)
+#define LCD_HEIGHT	(208)
+#endif
+
 #if LCD_MONOCHROME
 #define LCD_VRAM	(0x80000)
-#define LCD_VRAM_SIZE	(0x2580)
+#define LCD_VRAM_SIZE	((LCD_WIDTH) * (LCD_HEIGHT) / 8)
 #else
 #define LCD_VRAM	(0x10100000)
-#define LCD_VRAM_SIZE	((320 * 240) / 2)
+#define LCD_VRAM_SIZE	((LCD_WIDTH) * (LCD_HEIGHT) / 2)
 #endif
 
 #endif /* LCD_H */
