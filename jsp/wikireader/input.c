cd50299ea0a4a39ee9a76585f527f449314dec20
zecke23 <zecke23@974c56cd-8151-0410-98c4-7bb2f43fa8c1>
2009-03-06 06:25:17 +0000

    [toppers] Fix the compile...
    git-svn-id: https://wikipediardware.googlecode.com/svn/trunk@929 974c56cd-8151-0410-98c4-7bb2f43fa8c1

--- jsp/wikireader/input.c
+++ jsp/wikireader/input.c
@@ -1,7 +1,7 @@
 #include <input.h>
 #include "sample1.h"
 
-int wl_input_wait(struct wl_input_event *ev)
+int wl_input_wait(struct wl_input_event *ev, int sleep)
 {
 	ev->type = WL_INPUT_EV_TYPE_KEYBOARD;
 	syscall(serial_rea_dat(TASK_PORTID, &ev->key_event.keycode, 1));
