6fbbc1856e8c80a0d99d1a15ac4d5745646aedbd
zecke23 <zecke23@974c56cd-8151-0410-98c4-7bb2f43fa8c1>
2009-03-23 17:39:40 +0000

    [wiki-tools] Put the hash of the article in front of the URL
    This will be used by the split scripts to place the .blib and link
    files in the right directory for further processing.
    
    git-svn-id: https://wikipediardware.googlecode.com/svn/trunk@1029 974c56cd-8151-0410-98c4-7bb2f43fa8c1

--- host-tools/wiki-tools/src/ExtractArticleUrl.cc
+++ host-tools/wiki-tools/src/ExtractArticleUrl.cc
@@ -40,5 +40,5 @@ void ExtractArticleUrl::handleArticle(const Article& article)
         return;
 
     QTextStream stream(&m_file);
-    stream << "\%SETUP\%/" + article.title().title() << endl;
+    stream << article.hash() << " " << "\%SETUP\%/" + article.title().title() << endl;
 }
