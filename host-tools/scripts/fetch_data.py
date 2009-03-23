8657380e571bd93e9403ddb8f5a040eac15ed531
zecke23 <zecke23@974c56cd-8151-0410-98c4-7bb2f43fa8c1>
2009-03-23 17:40:30 +0000

    [host-tools] Log failed attempts fo fetch articles
    git-svn-id: https://wikipediardware.googlecode.com/svn/trunk@1033 974c56cd-8151-0410-98c4-7bb2f43fa8c1

--- host-tools/scripts/fetch_data.py
+++ host-tools/scripts/fetch_data.py
@@ -14,6 +14,8 @@ display = 99 - int(job_dir.rsplit('/', 1)[1])
 os.system("Xvfb :%d -noreset &" % display)
 os.environ['DISPLAY'] = ":%d" % display
 
+failed_urls = open("failed.urls", "w")
+
 
 def execute(hash, url):
     print "Getting %s" % url
@@ -28,6 +30,9 @@ for work in glob.glob("*.work"):
     file = open(work)
     for line in file:
         data = line[:-1].split(" ", 1)
-        execute(data[0], data[1])
+        try:
+            execute(data[0], data[1])
+        except:
+            print >> failed_urls, "%s %s" % (data[0], data[1])
 
 
