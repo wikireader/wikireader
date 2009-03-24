ca2e0bd4b8c1fb5a2fdc84b77b861f2f143a23c6
matt_hsu@openmoko.org <matt_hsu@openmoko.org@974c56cd-8151-0410-98c4-7bb2f43fa8c1>
2009-03-24 03:23:09 +0000

    [tests] Remove ncurses from the simulator, rename to tests
    We don't need ncurses for our unit tests and don't need
    to mess around with the terminal settings
    
    Acked-By: zecke
    
    git-svn-id: https://wikipediardware.googlecode.com/svn/trunk@1039 974c56cd-8151-0410-98c4-7bb2f43fa8c1

--- host-tools/simulator/tests/main.c
+++ host-tools/simulator/tests/main.c
@@ -3,6 +3,7 @@
     ncurses based simulator
 
     Copyright (c) 2008 Daniel Mack <daniel@caiaq.de>
+    Copyright (c) 2009 Matt Hsu <matt_hsu@openmoko.org>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -19,14 +20,11 @@
 */
 
 #include <stdio.h>
-#include <ncurses.h>
 
 #include <wikilib.h>
 #include <guilib.h>
 #include <msg.h>
 #include <input.h>
-
-static int loglevel = MSG_LEVEL_MAX;
 
 /* empty dummies - no framebuffer here */
 void fb_set_pixel(int x, int y, int val) {}
@@ -39,57 +37,13 @@ unsigned char *framebuffer = &framebuffer_data[0];
 int wl_input_wait(struct wl_input_event *ev, int sleep)
 {
 	ev->type = WL_INPUT_EV_TYPE_KEYBOARD;
-	ev->key_event.keycode = getch();
+	ev->key_event.keycode = getc(stdin);
 	ev->key_event.value = 1;
 	return 0;
 }
 
-void set_loglevel(int level)
-{
-	loglevel = level;
-}
-
-void msg(int level, const char *format, ...)
-{
-	va_list ap;
-	int attrs;
-
-	if (level > loglevel)
-		return;
-
-	switch (level) {
-	case MSG_DEBUG:
-		attrs = COLOR_PAIR(3);
-		break;
-	case MSG_ERROR:
-		attrs = COLOR_PAIR(2);
-		break;
-	default:
-		attrs = COLOR_PAIR(1);
-		break;
-	}
-
-        va_start(ap, format);
-	attron(attrs);
-	vwprintw(stdscr, format, ap);
-	attroff(attrs);
-	va_end(ap);
-}
-
 int main(int argc, char *argv[])
 {
-	initscr();
-	keypad(stdscr, TRUE);
-	noecho();
-	cbreak();
-	scrollok(stdscr, TRUE);
-	idlok(stdscr, TRUE);
-
-	start_color();
-	init_pair(1, COLOR_WHITE, COLOR_BLACK);
-	init_pair(2, COLOR_RED, COLOR_BLACK);
-	init_pair(3, COLOR_YELLOW, COLOR_BLACK);
-
 	wikilib_init();
 	guilib_init();
 	wikilib_run();
