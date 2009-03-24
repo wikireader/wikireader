bf74222a61b44021e635befe32808eebe1584b10
zecke23 <zecke23@974c56cd-8151-0410-98c4-7bb2f43fa8c1>
2009-03-24 14:08:57 +0000

    [host-tools] Untested timeout handling code...
    The idea is that we execute and wait and that the Unix alarm
    will interrupt us and then we will kill the process we have launched
    and wait should return. It should be as race free as it can get...
    
    this code is not tested...
    
    git-svn-id: https://wikipediardware.googlecode.com/svn/trunk@1057 974c56cd-8151-0410-98c4-7bb2f43fa8c1

--- host-tools/scripts/fetch_data.py
+++ host-tools/scripts/fetch_data.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import glob, os, sys, subprocess
+import glob, os, sys, subprocess, signal
 job_dir = sys.argv[1]
 
 
@@ -15,13 +15,41 @@ os.system("Xvfb :%d -noreset &" % display)
 os.environ['DISPLAY'] = ":%d" % display
 
 failed_urls = open("failed.urls", "w")
+current_pid = None
 
 class TimeOutException(Exception):
     pass
 
 
+def alarm_handler(signum, frame):
+    """
+    Kill the process with current_pid... this should help to get
+    run_command unstuck...
+    """
+    os.kill(current_pid, signal.SIGKILL)
+
 def run_command(cmd):
-    subprocess.check_call(cmd)
+    """
+    Execute a command. If the command is not finished with 120 seconds
+    the program will be killed with SIGKILL. If the return value of the
+    program is smaller than < 0 a timeout exception will be raised, if it
+    is > 0 a CalledProcessError will be raised.
+    """
+    global current_pid
+
+    # Launch and set an alarm... alarm_hnalder will kill
+    # current_pid
+    signal.alarm(120)
+    proc = subprocess.Popen(cmd)
+    current_pid = proc.pid
+    pid, sts = os.waipid(proc.pid, 0)
+
+    # cancel timer and error checking
+    signal.alarm(0)
+    if sts > 0:
+        raise subprocess.CalledProcessError()
+    elif sts < 0:
+        raise TimeOutException()
 
 def execute(hash, url):
     print "Getting %s" % url
