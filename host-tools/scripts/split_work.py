f18e30ddbb817a96895f107be56f4bd05d99a2e8
zecke23 <zecke23@974c56cd-8151-0410-98c4-7bb2f43fa8c1>
2009-03-25 02:50:13 +0000

    [host-tools] Generate a file which can be used by wget to download
    git-svn-id: https://wikipediardware.googlecode.com/svn/trunk@1065 974c56cd-8151-0410-98c4-7bb2f43fa8c1

--- host-tools/scripts/split_work.py
+++ host-tools/scripts/split_work.py
@@ -22,6 +22,7 @@ NUMBER_OF_JOBS = 6
 file = open("urls.fetch")
 current_number = 0
 current_file = None
+current_wget = None
 file_no = 0
 
 last_thread = {}
@@ -32,15 +33,20 @@ for thread in range(1, NUMBER_OF_SLAVES + 1):
 for line in file:
    if not current_file:
        file = "%.5d.work" % file_no
+       wget_file = "%.5d.wget" % file_no
        slave_nr = ((file_no % NUMBER_OF_SLAVES) + 1)
        slave = "zecke-%d" % slave_nr
        thread = "%d" % (last_thread[slave_nr] + 1)
+
        path = os.path.join(slave, thread, file)
+       wget_path = os.path.join(slave, thread, wget_file)
        current_file = open(path, "w")
+       current_wget = open(wget_path, "w")
        file_no = file_no + 1
        last_thread[slave_nr] = (last_thread[slave_nr] + 1) % NUMBER_OF_JOBS
 
    print >> current_file, line[:-1] 
+   print >> current_wget, line.split(" ", 1)[1][:-1]
    current_number = current_number + 1
    if current_number % ITEMS_PER_PACKAGE == 0:
        current_file.close()
