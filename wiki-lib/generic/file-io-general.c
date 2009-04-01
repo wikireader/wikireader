042fad8cf425ebab5fcb5498d0a28e3f72d43c22
john.cylee <john.cylee@974c56cd-8151-0410-98c4-7bb2f43fa8c1>
2009-04-01 21:16:15 +0000

    [wiki-lib] file-io-general.c: fix issue 2, dispell the monkey-foo black magic.
    git-svn-id: https://wikipediardware.googlecode.com/svn/trunk@1105 974c56cd-8151-0410-98c4-7bb2f43fa8c1

--- wiki-lib/generic/file-io-general.c
+++ wiki-lib/generic/file-io-general.c
@@ -85,7 +85,7 @@ int wl_seek(int fd, unsigned int pos)
 	struct wl_file *fp = file_list + fd;
 	unsigned int l_offset = pos & ~BLOCK_ALIGNMENT;
 
-	if (l_offset == fp->l_offset)
+	if (l_offset + BLOCK_SIZE == fp->l_offset)
 		goto success;
 
 	r = _wl_seek(fd, l_offset);
@@ -99,7 +99,7 @@ int wl_seek(int fd, unsigned int pos)
 
 success:
 	/* TODO: if seek beyond eof ? */
-	fp->bytes_available -= pos & BLOCK_ALIGNMENT;
+	fp->bytes_available = BLOCK_SIZE - pos & BLOCK_ALIGNMENT;
 	return 0;
 }
 
