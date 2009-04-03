2ef55e9d6464ebd7e8c4d48c470aa3f2f673e7cc
hsw@openmoko.com <hsw@openmoko.com@974c56cd-8151-0410-98c4-7bb2f43fa8c1>
2009-04-03 08:20:38 +0000

    [forth] New SIO names
    Also add input available function
    
    Signed-off-by: Christopher Hall <hsw@openmoko.com>
    
    
    git-svn-id: https://wikipediardware.googlecode.com/svn/trunk@1119 974c56cd-8151-0410-98c4-7bb2f43fa8c1

--- forth/sio.s
+++ forth/sio.s
@@ -1,4 +1,4 @@
-;;; simple serial I/O
+;;; sio - simple serial I/O
 
         .include "regs.inc"
 
@@ -20,13 +20,13 @@ RDBFx    = (1 << 0)
 ;;; print cr and lf
 ;;; input:
 ;;; output:
-        .global putcrlf
-putcrlf:
+        .global sio_put_crlf
+sio_put_crlf:
         pushn   %r0
         xld.w   %r0, 0x0d
-        xcall   putchar
+        xcall   sio_put_char
         xld.w   %r0, 0x0a
-        xcall   putchar
+        xcall   sio_put_char
         popn    %r0
         ret
 
@@ -34,11 +34,11 @@ putcrlf:
 ;;; print a space
 ;;; input:
 ;;; output:
-        .global putspace
-putspace:
+        .global sio_put_space
+sio_put_space:
         pushn   %r0
         xld.w   %r0, 0x20
-        xcall   putchar
+        xcall   sio_put_char
         popn    %r0
         ret
 
@@ -47,14 +47,14 @@ putspace:
 ;;; input:
 ;;;   r0 = char
 ;;; output:
-        .global putchar
-putchar:
+        .global sio_put_char
+sio_put_char:
         pushn   %r2
         xld.w   %r1, REG_EFSIF0_STATUS
-putchar_wait:
+sio_put_char_wait:
         ld.ub   %r2, [%r1]
         xand    %r2, TDBEx
-        jreq    putchar_wait
+        jreq    sio_put_char_wait
 
         xld.w   %r1, REG_EFSIF0_TXD
         ld.b	[%r1], %r0
@@ -65,17 +65,17 @@ putchar_wait:
 ;;; input:
 ;;;   r0 = address of '\0' terminated string
 ;;; output:
-        .global puts
-puts:
+        .global sio_put_string
+sio_put_string:
         pushn   %r1
         ld.w    %r1, %r0
-puts_loop:
+sio_put_string_loop:
         ld.ub   %r0, [%r1]+
         cmp     %r0, 0
-        jreq    puts_done
-        xcall   putchar
-        jp      puts_loop
-puts_done:
+        jreq    sio_put_string_done
+        xcall   sio_put_char
+        jp      sio_put_string_loop
+sio_put_string_done:
         popn    %r1
         ret
 
@@ -84,16 +84,16 @@ puts_done:
 ;;; input:
 ;;;   r0 = 4 bit number to print
 ;;; output:
-        .global putnibble
-putnibble:
+        .global sio_put_nibble
+sio_put_nibble:
         pushn   %r0
         xand    %r0, 0x0f
         xadd    %r0, '0'
         xcmp    %r0, '9'
-        jrle    putnibble_l1
+        jrle    sio_put_nibble_l1
         xadd    %r0, 'a' - '9' - 1
-putnibble_l1:
-        xcall   putchar
+sio_put_nibble_l1:
+        xcall   sio_put_char
         popn    %r0
         ret
 
@@ -102,40 +102,40 @@ putnibble_l1:
 ;;; input:
 ;;;   r0 = 32 bit number to print
 ;;; output:
-        .global puthex
-puthex:
+        .global sio_put_hex
+sio_put_hex:
         pushn    %r1
 
         ld.w    %r1, %r0
         xsrl    %r0, 28
-        xcall   putnibble
+        xcall   sio_put_nibble
 
         ld.w    %r0, %r1
         xsrl    %r0, 24
-        xcall   putnibble
+        xcall   sio_put_nibble
 
         ld.w    %r0, %r1
         xsrl    %r0, 20
-        xcall   putnibble
+        xcall   sio_put_nibble
 
         ld.w    %r0, %r1
         xsrl    %r0, 16
-        xcall   putnibble
+        xcall   sio_put_nibble
 
         ld.w    %r0, %r1
         xsrl    %r0, 12
-        xcall   putnibble
+        xcall   sio_put_nibble
 
         ld.w    %r0, %r1
         xsrl    %r0, 8
-        xcall   putnibble
+        xcall   sio_put_nibble
 
         ld.w    %r0, %r1
         xsrl    %r0, 4
-        xcall   putnibble
+        xcall   sio_put_nibble
 
         ld.w    %r0, %r1
-        xcall   putnibble
+        xcall   sio_put_nibble
 
         popn    %r1
         ret
@@ -145,16 +145,27 @@ puthex:
 ;;; input:
 ;;; output:
 ;;;   r0 = char
-        .global getchar
-getchar:
-        pushn   %r1
-        xld.w   %r1, REG_EFSIF0_STATUS
-getchar_wait:
-        ld.ub   %r0, [%r1]
-        xand    %r0, RDBFx
-        jreq    getchar_wait
-        popn    %r1
-
+        .global sio_get_char
+sio_get_char:
+        call    sio_input_available
+        or      %r0, %r0
+        jreq    sio_get_char
         xld.w   %r0, REG_EFSIF0_RXD
         ld.ub	%r0, [%r0]
+        ret
+
+
+;;; see if input is available
+;;; input:
+;;; output:
+;;;   r0 = 0 => not ready
+;;;        1 => ready
+        .global sio_input_available
+sio_input_available:
+        xld.w   %r0, REG_EFSIF0_STATUS
+        ld.ub   %r0, [%r0]
+        xand    %r0, RDBFx
+        jreq    sio_input_available_done
+        ld.w    %r0, 1
+sio_input_available_done:
         ret
