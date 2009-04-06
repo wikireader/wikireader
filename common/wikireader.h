fdf8dda2a0f2bf342564f5bd59f66e31dcfe8aa7
hsw@openmoko.com <hsw@openmoko.com@974c56cd-8151-0410-98c4-7bb2f43fa8c1>
2009-04-06 03:12:55 +0000

    Remove trailing whitespace
    Signed-off-by: Christopher Hall <hsw@openmoko.com>
    
    
    git-svn-id: https://wikipediardware.googlecode.com/svn/trunk@1166 974c56cd-8151-0410-98c4-7bb2f43fa8c1

--- common/wikireader.h
+++ common/wikireader.h
@@ -17,8 +17,8 @@ static inline void init_pins(void)
 	REG_P0_03_CFP = 0x05;
 
 	/* LCD controller */
-  	REG_P8_03_CFP = 0x15;
-  	REG_P9_47_CFP = 0x55;
+	REG_P8_03_CFP = 0x15;
+	REG_P9_47_CFP = 0x55;
 
 	/* board specific things */
 #if BOARD_S1C33E07
@@ -37,13 +37,13 @@ static inline void init_pins(void)
 
 	REG_MISC_PUP6 = (1 << 5);
 
-#elif BOARD_PRT33L17LCD 
+#elif BOARD_PRT33L17LCD
 	/* SDCARD CS# */
 	REG_P8_IOC8 = (1 << 3);
 	/* EEPROM WP# */
 	REG_P1_IOC1 = (1 << 4);
 #endif
-	
+
 	/* P50 & P52: CS lines */
 	REG_P5_IOC5 = 0x07;
 }
@@ -65,8 +65,8 @@ static inline void init_rs232(void)
 	/*
 	 * BRTRD = ((F[brclk] * DIVMD) / (2 * bps)) - 1;
 	 * where
-	 * 	F[brclk] = 48MHz
-	 * 	DIVMD = 1/8
+	 *	F[brclk] = 48MHz
+	 *	DIVMD = 1/8
 	 *	bps = 57600
 	 *
 	 *   = 51
@@ -80,65 +80,65 @@ static inline void init_rs232(void)
 }
 
 /* MRS command address for burst length=1, CAS latency = 2 */
-#define MRSREG          (*(volatile unsigned char *) 0x10000442)
-#define RAMDUMMY        (*(volatile unsigned char *) 0x10000000)
+#define MRSREG		(*(volatile unsigned char *) 0x10000442)
+#define RAMDUMMY	(*(volatile unsigned char *) 0x10000000)
 
 static inline void init_ram(void)
 {
 	int i;
 
-        /* P20-P27 functions */
-        REG_P2_03_CFP = 0x55;
-        REG_P2_47_CFP = 0x55;
-        REG_P5_03_CFP |= 0x80;
+	/* P20-P27 functions */
+	REG_P2_03_CFP = 0x55;
+	REG_P2_47_CFP = 0x55;
+	REG_P5_03_CFP |= 0x80;
 
-        /* P85 */
-        REG_P8_45_CFP &= 0x03;
+	/* P85 */
+	REG_P8_45_CFP &= 0x03;
 
 
-        /* disable write protection of clock registers */
-        REG_CMU_PROTECT = 0x96;
+	/* disable write protection of clock registers */
+	REG_CMU_PROTECT = 0x96;
 
-        /* switch on SDRAM clk */
-        REG_CMU_GATEDCLK0 |= 0x78;
+	/* switch on SDRAM clk */
+	REG_CMU_GATEDCLK0 |= 0x78;
 
-        /* re-enable write protection of clock registers */
-        REG_CMU_PROTECT = 0x00;
+	/* re-enable write protection of clock registers */
+	REG_CMU_PROTECT = 0x00;
 
-        /* enable SDRAMC application core */
-        REG_SDRAMC_APP = 0x8000000b;
+	/* enable SDRAMC application core */
+	REG_SDRAMC_APP = 0x8000000b;
 
-        /* set up SDRAM controller */
-        /* 4M x 16 bits x 1, 15 Trs/Trfc/Txsr cycles, 3 Tras, 2 Trp */
-        REG_SDRAMC_CTL = 0x37e1;
+	/* set up SDRAM controller */
+	/* 4M x 16 bits x 1, 15 Trs/Trfc/Txsr cycles, 3 Tras, 2 Trp */
+	REG_SDRAMC_CTL = 0x37e1;
 
-        /* disable RAM self-refresh, ... */
+	/* disable RAM self-refresh, ... */
 	REG_SDRAMC_REF = 0x8c | (1 << 23) | (0x7f << 16);
 
 	/* enter RAM setup mode */
-        REG_SDRAMC_INI = 0x14;
+	REG_SDRAMC_INI = 0x14;
 
-        /* SDRAM command sequence: PALL - REF - REF - MRS (for MT48LC16M16A2) */
-        REG_SDRAMC_INI = 0x12;  /* INIPRE */
-        RAMDUMMY = 0x0;         /* dummy write */
+	/* SDRAM command sequence: PALL - REF - REF - MRS (for MT48LC16M16A2) */
+	REG_SDRAMC_INI = 0x12;	/* INIPRE */
+	RAMDUMMY = 0x0;		/* dummy write */
 
-        REG_SDRAMC_INI = 0x12;  /* INIPRE */
-        RAMDUMMY = 0x0;         /* dummy write */
+	REG_SDRAMC_INI = 0x12;	/* INIPRE */
+	RAMDUMMY = 0x0;		/* dummy write */
 
-        for (i = 0; i < 2; i++) {
-                REG_SDRAMC_INI = 0x11;  /* INIREF */
-                RAMDUMMY = 0x0;         /* dummy write */
-        }
+	for (i = 0; i < 2; i++) {
+		REG_SDRAMC_INI = 0x11;	/* INIREF */
+		RAMDUMMY = 0x0;		/* dummy write */
+	}
 
-        REG_SDRAMC_INI = 0x14;  /* INIMRS */
-        MRSREG = 0x0;           /* dummy write */
+	REG_SDRAMC_INI = 0x14;	/* INIMRS */
+	MRSREG = 0x0;		/* dummy write */
 
 	/* exit RAM setup mode */
-        REG_SDRAMC_INI = 0x10;
+	REG_SDRAMC_INI = 0x10;
 }
 
 #if BOARD_PROTO1
-	#define enable_card_power()  do { REG_P3_P3D |=  (1 << 3); } while(0)
+	#define enable_card_power()  do { REG_P3_P3D |=	 (1 << 3); } while(0)
 	#define disable_card_power() do { REG_P3_P3D &= ~(1 << 3); } while(0)
 #else
 static inline void enable_card_power(void)
@@ -173,7 +173,7 @@ static inline void disable_card_power(void)
 #if BOARD_S1C33E07
 	#define SDCARD_CS_LO()	do { REG_PA_DATA &= ~(1 << 3); } while (0)
 	#define SDCARD_CS_HI()	do { REG_PA_DATA |=  (1 << 3); } while (0)
-	#define EEPROM_WP_HI()	do { REG_P2_P2D  =   (1 << 6); } while (0)
+	#define EEPROM_WP_HI()	do { REG_P2_P2D	 =   (1 << 6); } while (0)
 #elif BOARD_PROTO1
 	#define SDCARD_CS_LO()	do { REG_P5_P5D &= ~(1 << 0); } while (0)
 	#define SDCARD_CS_HI()	do { REG_P5_P5D |=  (1 << 0); } while (0)
@@ -181,8 +181,8 @@ static inline void disable_card_power(void)
 #elif BOARD_PRT33L17LCD
 	#define SDCARD_CS_LO()	do { REG_P8_P8D &= ~(1 << 4); } while (0)
 	#define SDCARD_CS_HI()	do { REG_P8_P8D |=  (1 << 4); } while (0)
-	#define EEPROM_WP_HI()	do { REG_P2_P2D  =  (1 << 6); } while (0)
-#endif 
+	#define EEPROM_WP_HI()	do { REG_P2_P2D	 =  (1 << 6); } while (0)
+#endif
 
 #endif /* WIKIREADER_H */
 
