63a496c43f3da9d2d59bc395e56dbc197687f58c
hsw@openmoko.com <hsw@openmoko.com@974c56cd-8151-0410-98c4-7bb2f43fa8c1>
2009-04-06 03:13:49 +0000

    Initial (untetested) PM25LV512 EEPROM driver
    Need to have board with actual chip for testing.
    
    Signed-off-by: Christopher Hall <hsw@openmoko.com>
    
    
    git-svn-id: https://wikipediardware.googlecode.com/svn/trunk@1170 974c56cd-8151-0410-98c4-7bb2f43fa8c1

--- bootloader/e07load/eeprom.c
+++ bootloader/e07load/eeprom.c
@@ -94,7 +94,7 @@ int write_eeprom (int fd, unsigned char *buf, ssize_t len, ssize_t offset)
 
 		spi_cs_lo(fd);
 		eeprom_write_page(fd, a + offset);
-		
+
 		cmdbuf[0] = SPI_WRITE;
 		cmdbuf[1] = xlen;
 		memcpy(cmdbuf + 2, buf + a, xlen);
@@ -207,7 +207,7 @@ int write_eeprom (int fd, unsigned char *buf, ssize_t len, ssize_t offset)
 
 	for (i = first_block; i <= last_block; i++)
 		eeprom_erase_block(fd, i);
-	
+
 	msg("writing %d bytes to EEPROM, offset 0x%x ", len, offset);
 	eeprom_write_enable(fd, 1);
 
@@ -216,7 +216,7 @@ int write_eeprom (int fd, unsigned char *buf, ssize_t len, ssize_t offset)
 	cmdbuf[0] = SPI_WRITE;
 	cmdbuf[1] = 6;
 	cmdbuf[2] = 0xad;
-	
+
 	if (offset & 1) {
 		cmdbuf[3] = (offset - 1) >> 16;
 		cmdbuf[4] = (offset - 1) >> 8;
@@ -238,7 +238,7 @@ int write_eeprom (int fd, unsigned char *buf, ssize_t len, ssize_t offset)
 
 	while (len > 0) {
 		eeprom_wait_ready(fd);
-		
+
 		/* write next 2 bytes */
 		spi_cs_lo(fd);
 		cmdbuf[0] = SPI_WRITE;
@@ -255,6 +255,170 @@ int write_eeprom (int fd, unsigned char *buf, ssize_t len, ssize_t offset)
 		}
 
 		len -= 2;
+	}
+
+	eeprom_write_enable(fd, 0);
+
+	msg("\n");
+	return 0;
+}
+
+
+#elif EEPROM_PM25LV512
+
+static void eeprom_wait_ready (int fd)
+{
+	char status;
+	unsigned char cmdbuf[3];
+
+	//usleep(8000);
+	//return;
+
+	do {
+		spi_cs_lo(fd);
+		cmdbuf[0] = SPI_WRITE;
+		cmdbuf[1] = 1;
+		cmdbuf[2] = 0x05;
+		write(fd, cmdbuf, 3);
+
+		cmdbuf[0] = SPI_READ;
+		cmdbuf[1] = 1;
+		write(fd, cmdbuf, 2);
+		read_blocking(fd, &status, 1);
+		spi_cs_hi(fd);
+	} while (0x01 == (status & 0x01));
+
+}
+
+
+static void eeprom_erase_block (int fd, int block)
+{
+	unsigned char cmdbuf[6];
+	static unsigned char block_erased[1 << 12] = { 0 };
+	int a = block << 12;
+
+	block &= (1 << 12) - 1;
+
+	if (block_erased[block])
+		return;
+
+	msg("erasing 4k block @addr 0x%08x\n", a);
+	eeprom_write_enable(fd, 1);
+
+	eeprom_wait_ready(fd);
+
+	spi_cs_lo(fd);
+	cmdbuf[0] = SPI_WRITE;
+	cmdbuf[1] = 4;
+	cmdbuf[2] = 0xd7;
+	cmdbuf[3] = a >> 16;
+	cmdbuf[4] = a >> 8;
+	cmdbuf[5] = a & 0xff;
+	write(fd, cmdbuf, sizeof(cmdbuf));
+	spi_cs_hi(fd);
+	usleep(100 * 1000);
+
+	eeprom_wait_ready(fd);
+
+	//block_erased[block] = 1;
+}
+
+
+static void eeprom_set_block_protection (int fd)
+{
+	unsigned char cmdbuf[4];
+
+	/* write configuration register */
+	spi_cs_lo(fd);
+	cmdbuf[0] = SPI_WRITE;
+	cmdbuf[1] = 2;
+	cmdbuf[2] = 0xf1;
+	cmdbuf[3] = 0x00;
+	write(fd, cmdbuf, 3);
+	spi_cs_hi(fd);
+
+	/* write status register */
+	spi_cs_lo(fd);
+	cmdbuf[0] = SPI_WRITE;
+	cmdbuf[1] = 2;
+	cmdbuf[2] = 0x01;
+	cmdbuf[3] = 0x02;
+	write(fd, cmdbuf, 4);
+	spi_cs_hi(fd);
+}
+
+int write_eeprom (int fd, unsigned char *buf, ssize_t len, ssize_t offset)
+{
+	unsigned char cmdbuf[8];
+	int i, first_block, last_block;
+
+	if (len & 1)
+		len++;
+
+	spi_cs_hi(fd);
+	eeprom_set_block_protection(fd);
+	eeprom_write_enable(fd, 1);
+
+	/* erase all 4k block we will touch */
+	first_block = offset >> 12;
+	last_block = (offset + len) >> 12;
+
+	for (i = first_block; i <= last_block; i++)
+		eeprom_erase_block(fd, i);
+
+	msg("writing %d bytes to EEPROM, offset 0x%x ", len, offset);
+	eeprom_write_enable(fd, 1);
+
+	/* issue 1st AII command */
+	spi_cs_lo(fd);
+	cmdbuf[0] = SPI_WRITE;
+	cmdbuf[1] = 6;
+	cmdbuf[2] = 0x02;
+
+	if (offset & 1) {
+		cmdbuf[3] = (offset - 1) >> 16;
+		cmdbuf[4] = (offset - 1) >> 8;
+		cmdbuf[5] = (offset - 1) & 0xff;
+		cmdbuf[6] = 0x55;
+		cmdbuf[7] = *buf++;
+		--len;
+		++offset;
+	} else {
+		cmdbuf[3] = offset >> 16;
+		cmdbuf[4] = offset >> 8;
+		cmdbuf[5] = offset & 0xff;
+		cmdbuf[6] = *buf++;
+		cmdbuf[7] = *buf++;
+		len -= 2;
+		offset += 2;
+	}
+
+	write(fd, cmdbuf, sizeof(cmdbuf));
+	spi_cs_hi(fd);
+
+	while (len > 0) {
+		eeprom_wait_ready(fd);
+
+		/* write next 2 bytes */
+		spi_cs_lo(fd);
+		cmdbuf[0] = SPI_WRITE;
+		cmdbuf[1] = 6;
+		cmdbuf[2] = 0x02;
+		cmdbuf[3] = offset >> 16;
+		cmdbuf[4] = offset >> 8;
+		cmdbuf[5] = offset & 0xff;
+		cmdbuf[6] = *buf++;
+		cmdbuf[7] = *buf++;
+		write(fd, cmdbuf, 8);
+		spi_cs_hi(fd);
+		if ((len & 0x7f) == 0) {
+			printf(".");
+			fflush(stdout);
+			fflush(stderr);
+		}
+
+		len -= 2;
+		offset += 2;
 	}
 
 	eeprom_write_enable(fd, 0);
@@ -305,7 +469,7 @@ int verify_eeprom (int fd, unsigned char *buf, ssize_t len, ssize_t offset)
 		msg(".");
 		fflush(stdout);
 		fflush(stderr);
-	
+
 		if (memcmp(buf + a, verify_buf + a, i) != 0) {
 			msg("\n");
 			error("EEPROM verify failed in chunk @offset 0x%x, len = %d!\n", a, i);
@@ -318,7 +482,7 @@ int verify_eeprom (int fd, unsigned char *buf, ssize_t len, ssize_t offset)
 
 		a += i;
 	}
-	
+
 	msg(" ok.\n");
 	free(verify_buf);
 	return 0;
