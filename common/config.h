363befa5c17ea1a2597d009c9b10dc3d0b994a76
hsw@openmoko.com <hsw@openmoko.com@974c56cd-8151-0410-98c4-7bb2f43fa8c1>
2009-04-08 10:07:07 +0000

    [common] add BOARD_SAMO_A1 to configuration
    Also added error message if no boards are defined.
    
    Signed-off-by: Christopher Hall <hsw@openmoko.com>
    
    
    git-svn-id: https://wikipediardware.googlecode.com/svn/trunk@1199 974c56cd-8151-0410-98c4-7bb2f43fa8c1

--- common/config.h
+++ common/config.h
@@ -4,14 +4,23 @@
 //#define BOARD_S1C33E07 1
 //#define BOARD_PRT33L17LCD 1
 #define BOARD_PROTO1 1
+//#define BOARD_SAMO_A1 1
 
 #define LCD_MONOCHROME 1
 //#define POWER_MANAGEMENT 1
 
-#if BOARD_PROTO1
+#if BOARD_SAMO_A1
+
+	#define EEPROM_PM25LV512 1
+//	#define EEPROM_SST25VF040 1
+//	#define SDRAM_EM48AM1684VBD 1
+
+#elif BOARD_PROTO1
 	#define EEPROM_SST25VF040 1
-#else
+#elif BOARD_S1C33E07 || BOARD_PRT33L17LCD
 	#define EEPROM_MP45PE80 1
+#else
+#error "Unsupported board"
 #endif
 
 #endif /* CONFIG_H */
