1b8a644e706174fd68b279332b20d0dea06a605c
zonque <zonque@974c56cd-8151-0410-98c4-7bb2f43fa8c1>
2009-04-09 19:25:02 +0000

    [fatfs] don't call init_pins() from the mmc layer.
    Don't know what it does there, and it crashes the bootloader on my board.
    
    
    git-svn-id: https://wikipediardware.googlecode.com/svn/trunk@1217 974c56cd-8151-0410-98c4-7bb2f43fa8c1

--- fatfs/src/mmc.c
+++ fatfs/src/mmc.c
@@ -385,7 +385,6 @@ DSTATUS disk_initialize (
 	if (drv) return STA_NOINIT;			/* Supports only single drive */
 //	if (Stat & STA_NODISK) return Stat;		/* No card in the socket */
 
-	init_pins();
 	REG_SPI_CTL1 = 0x03 | (7 << 10) | (0 << 4);
 
 	power_on();					/* Force socket power on */
