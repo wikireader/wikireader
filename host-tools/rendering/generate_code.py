893eee0a30b1a2185b4c64fd97a24d4d712e939d
zecke23 <zecke23@974c56cd-8151-0410-98c4-7bb2f43fa8c1>
2009-04-10 11:12:50 +0000

    [rendering] The current approach does not yield a good result...
    This is the path to canossa, instead of sinking more time into
    the current huffman code approach to introduce dictionaries and
    group more things together we will opt for another simple solution.
    
    Currently it is plain text, with a special marker for a chain of
    glyphs, x- and y-position and font changes.
    
    git-svn-id: https://wikipediardware.googlecode.com/svn/trunk@1229 974c56cd-8151-0410-98c4-7bb2f43fa8c1

--- host-tools/rendering/generate_code.py
+++ host-tools/rendering/generate_code.py
@@ -20,26 +20,13 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import bitwriter
 import sys
-import huffmanCode
 import fontmap
 import textrun
-import codetree
 import optparse
 import os
 import gzip
 import glob
-
-# Globals
-glyph_map = {}
-font_map = {}
-huffman_fonts = {}
-huffman_glyphs = {}
-huffman_x = {}
-huffman_y = {}
-huffman_length = {}
-
 
 def determine_by_occurence(occurences):
     """Count the occurcences of something"""
@@ -61,116 +48,40 @@ def prepare_run(text_runs, glyph_occurences, font_occurences, x_occurences, y_oc
 
     # Sort by y position
     text_runs.sort(textrun.TextRun.cmp)
-
-    global glyph_map, font_map
-    glyph_map = determine_by_occurence(glyph_occurences)
-    font_map = determine_by_occurence(font_occurences)
-
-    # huffman foo...
-    global huffman_fonts, huffman_glyphs, huffman_x, huffman_y, huffman_length
-    font_input = []
-    for font in font_occurences:
-        font_input.append((font_occurences[font]/len(font_occurences), font))
-    huffman_fonts = huffmanCode.createCodeWordMap(huffmanCode.makeHuffTree(font_input))
-
-    glyph_input = []
-    for glyph in glyph_occurences:
-        glyph_input.append((glyph_occurences[glyph]/len(glyph_occurences), glyph))
-    huffman_glyphs = huffmanCode.createCodeWordMap(huffmanCode.makeHuffTree(glyph_input))
-
-    x_input = []
-    for x in x_occurences:
-        x_input.append((x_occurences[x]/len(x_occurences), x))
-    huffman_x = huffmanCode.createCodeWordMap(huffmanCode.makeHuffTree(x_input))
-
-    y_input = []
-    for y in y_occurences:
-        y_input.append((y_occurences[y]/len(y_occurences), y))
-    huffman_y = huffmanCode.createCodeWordMap(huffmanCode.makeHuffTree(y_input))
-
-    length_input = []
-    for length in length_occurences:
-        length_input.append((length_occurences[length]/len(length_occurences), length))
-    huffman_length = huffmanCode.createCodeWordMap(huffmanCode.makeHuffTree(length_input))
-
     return text_runs
 
 def write_to_file(text_runs, fonts, auto_kern_bit):
     """
     A function saving the text runs and hoping autokern will do its job
 
-
-    # The bitcode.....
-    0    - Paragraph
-    1    - Font Change
-
-    Parapgraph:
-        [0,1] - 0 no y change, 1 x and y change
-        number[number] 
-
-    Font:
-        Huffman code of the font
     """
 
-    def write_header(file):
-        """
-        Write out the header, the header is the code used
-        for this one article
-        """
-        def write_foo(file, dict):
-            tree = codetree.CodeTree()
-            for word in dict.keys():
-                tree.addCodeWord(dict[word], word)
-            tree.writeTo(file)
-        write_foo(file, huffman_x)
-        write_foo(file, huffman_y)
-        write_foo(file, huffman_length)
-        write_foo(file, huffman_glyphs)
-
-        tree = codetree.CodeTree()
-        for word in huffman_fonts.keys():
-            tree.addCodeWord(huffman_fonts[word], fonts[word])
-        tree.writeTo(file)
-    
-    def write_pending_bit(writer, run):
+    def write_pending_bit(file, run):
         """
         The text run is sorted by paragrah and all glyphs of
         one paragraph are on the same line and have roughly the
         same height.
         """
 
-        writer.write_bit(0)
-        writer.write_bits(huffman_x[run.first_x])
-        writer.write_bits(huffman_y[run.first_y])
+        if run.first_y == 0:
+            file.write("%d," % (run.first_x))
+        else:
+            file.write("%d_%d," % (run.first_x, run.first_y))
  
-        writer.write_bits(huffman_length[len(run.glyphs)])
-
         list = []
-        last_glyph = None
         for glyph in run.glyphs:
-            huffman_glyph = huffman_glyphs[glyph['glyph']]
-            writer.write_bits(huffman_glyph)
-            last_glyph = glyph
-
-    # Code
-    write_header(auto_kern_bit)
+            list.append(glyph['glyph'])
+        file.write("-".join(list))
 
     last_font = None
-    writer = bitwriter.BitWriter()
     for text_run in text_runs:
         # we migt have a new font now
         font = text_run.font
         if last_font != font:
-            writer.write_bit(1)
-            writer.write_bits(huffman_fonts[text_run.font])
+            auto_kern_bit.write("f%s," % fonts[font])
             last_font = font
 
-        write_pending_bit(writer, text_run)
-
-
-
-    bytes = writer.finish()
-    auto_kern_bit.write("".join(bytes))
+        write_pending_bit(auto_kern_bit, text_run)
 
 
 def parse():
