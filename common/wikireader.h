50178a402212c2864887075e90e9dc37e4da82b4
matt_hsu@openmoko.org <matt_hsu@openmoko.org@974c56cd-8151-0410-98c4-7bb2f43fa8c1>
2009-04-15 07:47:28 +0000

    [common] Reface the serial port initialization.
    - tidy up serial port #0 initialization,
    - provide re-usable routine for all serial ports initialization.
    
    git-svn-id: https://wikipediardware.googlecode.com/svn/trunk@1290 974c56cd-8151-0410-98c4-7bb2f43fa8c1

--- common/wikireader.h
+++ common/wikireader.h
@@ -34,34 +34,31 @@
 #define TFT_CTL1_LO()   do { REG_P8_P8D &= ~(1 << 3); } while (0)
 #define TFT_CTL1_HI()   do { REG_P8_P8D |=  (1 << 3); } while (0)
 
-static inline void init_rs232(void)
+#define MCLK 		48000000
+#define DIV 		8
+
+#define CALC_BAUD(fbrclk, divmd, bps)  ((fbrclk / divmd)/(2 * bps) - 1)
+
+#define SET_BRTRD(ch, value) 	do {		\
+									REG_EFSIF##ch##_BRTRDM = value >> 8;	\
+									REG_EFSIF##ch##_BRTRDL = value & 0xff; \
+								} while(0)
+
+#define SERIAL_8N1 (NO_PARx | ONE_STPBx | INT_CLKx | EIGHT_BIT_ASYNx)
+
+static inline void init_rs232_ch0(void)
 {
 	/* serial line 0: 8-bit async, no parity, internal clock, 1 stop bit */
-	REG_EFSIF0_CTL = 0xc3;
+	REG_EFSIF0_CTL = TXENx | RXENx | SERIAL_8N1;
 
 	/* DIVMD = 1/8, General I/F mode */
-	REG_EFSIF0_IRDA = 0x10;
-
-	/* by default MCLKDIV = 0 which means that the internal MCLK is OSC/1,
-	 * where OSC = OSC3 as OSCSEL[1:0] = 00b
-	 * Hence, MCLK is 48MHz */
+	REG_EFSIF0_IRDA = DIVMD_8x | IRMD_GEN_IFx;
 
 	/* set up baud rate timer reload data */
-	/*
-	 * BRTRD = ((F[brclk] * DIVMD) / (2 * bps)) - 1;
-	 * where
-	 *      F[brclk] = 48MHz
-	 *      DIVMD = 1/8
-	 *      bps = 57600
-	 *
-	 *   = 51
-	 */
-
-	REG_EFSIF0_BRTRDL = 51 & 0xff;
-	REG_EFSIF0_BRTRDM = 51 >> 8;
+	SET_BRTRD(0, CALC_BAUD(MCLK, DIV, 57600));
 
 	/* baud rate timer: run! */
-	REG_EFSIF0_BRTRUN = 0x01;
+	REG_EFSIF0_BRTRUN |= BRTRUN_STARx;
 }
 
 #ifdef INCLUDED_FROM_KERNEL
