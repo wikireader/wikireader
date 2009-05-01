99bd4f72787062c77d60f0afd0a76c401ca982c6
wolfgang@openmoko.com <wolfgang@openmoko.com@974c56cd-8151-0410-98c4-7bb2f43fa8c1>
2009-05-01 14:29:26 +0000

    write more sections to WOM file
    git-svn-id: https://wikipediardware.googlecode.com/svn/trunk@1479 974c56cd-8151-0410-98c4-7bb2f43fa8c1

--- wom_creator/wom_creator.c
+++ wom_creator/wom_creator.c
@@ -10,13 +10,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <time.h>
 #include "msg.h"
 #include "wom_file_format.h"
 
-#define DBG_WOM_CREATOR 1
+#define DBG_WOM_CREATOR 0
 
 #define STR_BUF_LEN	4096 // arbitrary length, used for parsing .work and .blib files, paths, etc.
 #define NUM_BITMAP_SLOTS	2048 // enough for the ca. 1600 characters we use in 4 Liberation fonts
+#define NUM_TITLE_SLOTS		5000 // tbd: needs to become dynamic, support up to 800,000 or so
 
 typedef struct font_bitmaps
 {
@@ -25,13 +27,36 @@ typedef struct font_bitmaps
 	int32_t advance_x, advance_y, bitmap_left_bearing, bitmap_top_bearing, glyph_height, glyph_width;
 	int pbm_width, pbm_height;
 	uint8_t bits[64];
+	uint32_t bitmaps_file_off; // offset into the bitmaps in the output file, to where this bitmap is located (0 if bitmap skipped)
 } font_bitmaps_t;
 font_bitmaps_t font_bitmaps[NUM_BITMAP_SLOTS];
 int32_t num_bitmaps_avail = NUM_BITMAP_SLOTS;
 int32_t num_bitmaps_used = 0;
 
+typedef struct article_titles
+{
+	char title[256]; // longest title in the 200810 dump is 214 characters
+	char hash[64]; // hash is 40-bytes hex string
+	char blib_path[256];
+	uint32_t index_file_off; // offset into the index in the output file, to where the index entry for this article is located
+} article_titles_t;
+article_titles_t article_titles[NUM_TITLE_SLOTS];
+int32_t num_titles_avail = NUM_TITLE_SLOTS;
+int32_t num_titles_used = 0;
+
+typedef struct stats
+{
+	time_t start_time, stop_time;
+	long unsigned int num_work_files;
+	long unsigned int max_chars_in_title;
+	long unsigned int min_chars_in_title;
+	long unsigned int num_characters;
+	long unsigned int num_skipped_chars;
+	long unsigned int max_characters_per_article;
+	long unsigned int min_characters_per_article;
+} stats_t;
+
 static int load_bitmap(font_bitmaps_t* font_bitmaps, int num_bitmaps_avail, int* num_bitmaps_used, const char* work_path_base, const char* font_and_size, int font_code);
-static int ftell_size(const char * file, size_t * size);
 static int fscan_num(const char* path, int32_t* number);
 
 int main(int argc, char** argv)
@@ -41,12 +66,16 @@ int main(int argc, char** argv)
 	FILE* blib_f = 0;
 	uint8_t wom_page_buf[WOM_PAGE_SIZE];
 	char line_buf[STR_BUF_LEN], hash[STR_BUF_LEN], article_title[STR_BUF_LEN], work_path_base[STR_BUF_LEN], font_and_size[STR_BUF_LEN];
-	int cur_work_arg, i, x_coord, y_coord, font_code;
+	uint32_t cur_page, index_first_page, bitmaps_first_page, articles_first_page, cur_dest_off, dest_file_len, last_index_off, last_bitmap_off;
+	char blib_path[256];
+	int cur_work_arg, i, j, x_coord, y_coord, font_code;
+	stats_t stats = {0};
 
 	if (argc < 3) {
 		printf("\nUsage: wom_creator OUTPUT WORK...\n\n");
 		return 1;
 	}
+	stats.start_time = time(0);
 	dest_f = fopen(argv[1], "w+b");
 	if (!dest_f) {
 		fprintf(stderr, "Error opening output file '%s'\n", argv[1]);
@@ -68,6 +97,7 @@ int main(int argc, char** argv)
 			fprintf(stderr, "X Error opening .work file '%s'\n", argv[cur_work_arg]);
 			continue;
 		}
+		stats.num_work_files++;
 		while (fgets(line_buf, sizeof(line_buf), work_f)) {
 			// Note that multiple article titles can point to the same blib file (same article content).
 			// For example in 002808.work, there are 1000 titles but only 962 unique article contents.
@@ -77,11 +107,23 @@ int main(int argc, char** argv)
 				fprintf(stderr, "X Error parsing .work line '%s'.\n", line_buf);
 				continue;
 			}
-			sprintf(line_buf, "%sarticles/%c/%.2s/%s.blib", work_path_base, hash[0], &hash[1], hash);
-			printf("O Processing article '%s' in '%s'\n", article_title, line_buf);
-			blib_f = fopen(line_buf, "r");
+			if (num_titles_used >= num_titles_avail) {
+				fprintf(stderr, "X Ran out of title buffer.\n");
+				break;
+			}
+			sprintf(blib_path, "%sarticles/%c/%.2s/%s.blib", work_path_base, hash[0], &hash[1], hash);
+
+			strcpy(article_titles[num_titles_used].title, article_title);
+			strcpy(article_titles[num_titles_used].hash, hash);
+			strcpy(article_titles[num_titles_used].blib_path, blib_path);
+			num_titles_used++;
+
+			DP(DBG_WOM_CREATOR, (MSG_INFO, "O Processing article '%s' in '%s'\n", article_title, blib_path));
+			stats.min_chars_in_title = stats.min_chars_in_title ? min(stats.min_chars_in_title, strlen(article_title)) : strlen(article_title);
+			stats.max_chars_in_title = max(stats.max_chars_in_title, strlen(article_title));
+			blib_f = fopen(blib_path, "r");
 			if (!blib_f) {
-				fprintf(stderr, "X Error opening blib file '%s' (article '%s').\n", line_buf, article_title);
+				fprintf(stderr, "X Error opening blib file '%s' (article '%s').\n", blib_path, article_title);
 				continue;
 			}
 			while (fgets(line_buf, sizeof(line_buf), blib_f)) {
@@ -90,8 +132,10 @@ int main(int argc, char** argv)
 					fprintf(stderr, "X Error parsing .blib line '%s'.\n", line_buf);
 					continue;
 				}
+				stats.num_characters++;
 				if (strncmp(font_and_size, "Liberation_Sans", 15 /* strlen */)) {
-					printf("X Skipping non-'Liberation Sans' character ('%s').\n", font_and_size);
+					stats.num_skipped_chars++;
+					DP(DBG_WOM_CREATOR, (MSG_INFO, "X Skipping non-'Liberation Sans' character ('%s').\n", font_and_size));
 					continue;
 				}
 
@@ -99,23 +143,215 @@ int main(int argc, char** argv)
 					printf("X Error loading font bitmap.\n");
 					continue;
 				}
-// 1/fonts/Liberation_Sans_9/214/
-// advance_c, advance_y, bitmap_left_bearing, bitmap_top_bearing, glyph_height, glyph_width, bitmap.pbm
 			}
 			fclose(blib_f);
-break; // temporarily only one article
+//break; // temporarily only one article
 		}
 		fclose(work_f);
 		work_f = 0;
 	}
+
 	//
 	// 2. Write index (ca. 1% of total output size)
 	//
+	{
+		int16_t uri_len;
+
+		cur_page = 1; // page 0 is for header
+		index_first_page = cur_page;
+		if (fseek(dest_f, cur_page * WOM_PAGE_SIZE, SEEK_SET))
+			fprintf(stderr, "X Error seeking in file.\n");
+		for (i = 0; i < num_titles_used; i++) {
+			// offset_into_articles unknown and can only be written after 2nd run over articles below
+			article_titles[i].index_file_off = ftell(dest_f);
+			if (fseek(dest_f, sizeof(uint32_t), SEEK_CUR))
+				fprintf(stderr, "X Error seeking in file.\n");
+			// write uri_len
+			uri_len = strlen(article_titles[i].title);
+			if (fwrite(&uri_len, 1 /* size */, sizeof(uri_len), dest_f) != sizeof(uri_len))
+				fprintf(stderr, "X Error writing file.\n");
+			// write abbreviated_uri[]
+			if (fwrite(article_titles[i].title, 1 /* size */, uri_len, dest_f) != uri_len)
+				fprintf(stderr, "X Error writing file.\n");
+		}
+		last_index_off = ftell(dest_f);
+		cur_page = (last_index_off + WOM_PAGE_SIZE - 1) / WOM_PAGE_SIZE;
+	}
+	//
 	// 3. Write bitmaps (1-5% of total output size)
+	//
+	{
+		wom_bitmap_t* wom_bitmap = (wom_bitmap_t*) wom_page_buf;
+		int bitmap_size;
+
+		bitmaps_first_page = cur_page;
+		if (fseek(dest_f, cur_page * WOM_PAGE_SIZE, SEEK_SET))
+			fprintf(stderr, "X Error seeking in file.\n");
+		for (i = 0; i < num_bitmaps_used; i++) {
+			if (!font_bitmaps[i].pbm_width || !font_bitmaps[i].pbm_height
+			    || font_bitmaps[i].pbm_width >= 256 || font_bitmaps[i].pbm_height >= 256) {
+				fprintf(stderr, "X Bitmap size problem (width %d height %d).\n", font_bitmaps[i].pbm_width, font_bitmaps[i].pbm_height);
+				continue;
+			}
+			font_bitmaps[i].bitmaps_file_off = ftell(dest_f);
+			wom_bitmap->width = font_bitmaps[i].pbm_width;
+			wom_bitmap->height = font_bitmaps[i].pbm_height;
+			bitmap_size = (wom_bitmap->width + 7)/8 * wom_bitmap->height;
+			memcpy(wom_bitmap->bits, font_bitmaps[i].bits, bitmap_size);
+			if (fwrite(wom_bitmap, 1 /* size */, 2*sizeof(uint8_t) + bitmap_size, dest_f) != 2*sizeof(uint8_t) + bitmap_size)
+				fprintf(stderr, "X Error writing file.\n");
+		}
+		last_bitmap_off = ftell(dest_f);
+		cur_page = (last_bitmap_off + WOM_PAGE_SIZE - 1) / WOM_PAGE_SIZE;
+	}
 	//
 	// 4. Write articles (>90% of total output size)
 	//
+	{
+		uint32_t article_start_off;
+		uint8_t cur_display_x;
+		uint32_t cur_display_y;
+
+		articles_first_page = cur_page;
+		if (fseek(dest_f, cur_page * WOM_PAGE_SIZE, SEEK_SET))
+			fprintf(stderr, "X Error seeking in file.\n");
+		for (i = 0; i < num_titles_used; i++) {
+			if (!article_titles[i].index_file_off) {
+				fprintf(stderr, "X No index for this article?\n");
+				continue;
+			}
+			article_start_off = ftell(dest_f);
+
+			// Open .blib file and iterate over characters
+			blib_f = fopen(article_titles[i].blib_path, "r");
+			if (!blib_f) {
+				fprintf(stderr, "X Error opening blib file '%s' (article '%s').\n",
+					article_titles[i].blib_path, article_titles[i].title);
+				continue;
+			}
+			cur_display_x = 0;
+			cur_display_y = 0;
+			while (fgets(line_buf, sizeof(line_buf), blib_f)) {
+				if (!strcmp(line_buf, "0,0,0,0\n")) continue;
+				if (sscanf(line_buf, "%i,%i,%[^,],%i", &x_coord, &y_coord, font_and_size, &font_code) != 4) {
+					fprintf(stderr, "X Error parsing .blib line '%s'.\n", line_buf);
+					continue;
+				}
+				if (x_coord < 0 || x_coord >= 240 || y_coord < 0) {
+					fprintf(stderr, "X x/y out of bounds (%i/%i)\n", x_coord, y_coord);
+					continue;
+				}
+				for (j = 0; j < num_bitmaps_used; j++) {
+					if (!strcmp(font_bitmaps[j].font_size, font_and_size)
+					    && font_bitmaps[j].font_code == font_code)
+						break;
+				}
+				if (j >= num_bitmaps_used) {
+					DP(DBG_WOM_CREATOR, (MSG_INFO, "X Skipping missing glyph.\n"));
+					continue;
+				}
+				// tbd: we should make y ascending only, determine the baseline etc.
+				if (y_coord + 128 < cur_display_y) {
+					DP(1, (MSG_INFO, "X y_coord out of range (y_coord %i cur_display_y %i)\n", y_coord, cur_display_y));
+					continue;
+				}
+				DP(y_coord >= cur_display_y + 128, (MSG_INFO, "X Long empty section (%i rows)\n", y_coord - cur_display_y));
+// tbd: if y coordinates are ascending only, we may want to cut out large empty vertical spaces
+				while (y_coord >= cur_display_y + 128) {
+					wom_page_buf[0] = WOM_Y_ADVANCE_ONLY;
+					if (fwrite(wom_page_buf, 1 /* size */, sizeof(uint8_t), dest_f) != sizeof(uint8_t)) {
+						fprintf(stderr, "X Error writing to file.\n");
+						continue;
+					}
+					cur_display_y += 127;
+				}
+				if (x_coord >= cur_display_x)
+					wom_page_buf[0] = x_coord - cur_display_x;
+				else
+					wom_page_buf[0] = 256 - (cur_display_x - x_coord);
+				cur_display_x = x_coord;
+				wom_page_buf[1] = (int8_t) (y_coord - cur_display_y);
+				cur_display_y = y_coord;
+				// write uint32_t offset after x and y coordinates
+				*(uint32_t*)&wom_page_buf[2] = font_bitmaps[j].bitmaps_file_off;
+				if (fwrite(wom_page_buf, 1 /* size */, 6, dest_f) != 6) {
+					fprintf(stderr, "X Error writing to file.\n");
+					continue;
+				}
+			}
+			// write end of article marker
+			wom_page_buf[0] = WOM_END_OF_ARTICLE; // delta_x value to mark end of article
+			if (fwrite(wom_page_buf, 1 /* size */, sizeof(uint8_t), dest_f) != sizeof(uint8_t)) {
+				fprintf(stderr, "X Error writing to file.\n");
+				continue;
+			}
+				
+			// write offset of this article into index array earlier in the file
+			cur_dest_off = ftell(dest_f);
+			if (fseek(dest_f, article_titles[i].index_file_off, SEEK_SET)) {
+				fprintf(stderr, "X Error seeking in file.\n");
+				continue;
+			}
+			if (fwrite(&article_start_off, 1 /* size */, sizeof(article_start_off), dest_f) != sizeof(article_start_off)) {
+				fseek(dest_f, cur_dest_off, SEEK_SET);
+				fprintf(stderr, "X Error writing to file.\n");
+				continue;
+			}
+			if (fseek(dest_f, cur_dest_off, SEEK_SET)) {
+				fprintf(stderr, "X Error seeking in file.\n");
+				continue;
+			}
+		}
+	}
+	//
+	// 5. Write header
+	//
+	{
+		wom_header_t* wom_hdr = (wom_header_t*) wom_page_buf;
+		memset(wom_page_buf, 0, sizeof(wom_page_buf));
+		strcpy(wom_hdr->mime_type, WOM_MIME_TYPE);
+		wom_hdr->binary_magic = WOM_BINARY_MAGIC;
+		wom_hdr->file_format_version = WOM_FORMAT_VERSION;
+		wom_hdr->page_size = WOM_PAGE_SIZE;
+		wom_hdr->index_first_page = index_first_page;
+		wom_hdr->index_num_pages = bitmaps_first_page - index_first_page;
+		wom_hdr->index_num_entries = num_titles_used;
+		wom_hdr->bitmaps_first_page = bitmaps_first_page;
+		wom_hdr->bitmaps_num_pages = articles_first_page - bitmaps_first_page;
+		wom_hdr->bitmaps_num_entries = num_bitmaps_used;
+		wom_hdr->articles_first_page = articles_first_page;
+		wom_hdr->articles_num_pages = cur_page - articles_first_page;
+
+		if (fseek(dest_f, 0 /* offset */, SEEK_SET))
+			fprintf(stderr, "X Error seeking in file.\n");
+		if (fwrite(wom_page_buf, 1 /* size */, sizeof(wom_page_buf), dest_f) != sizeof(wom_page_buf))
+			fprintf(stderr, "X Error writing file.\n");
+	}
+		
+	// determine file length
+	fseek(dest_f, 0, SEEK_END);
+	dest_file_len = ftell(dest_f);
+
 	fclose(dest_f);
+	stats.stop_time = time(0);
+
+	//
+	// 6. Printf stats.
+	//
+	printf("\nwom_creator stats for this run:\n\n");
+	printf("  duration of run:                     %lu seconds\n", stats.stop_time - stats.start_time);
+	printf("  num_work_files:                      %lu\n", stats.num_work_files);
+	printf("  num_titles:                          %lu\n", num_titles_used);
+	printf("  article title length (min/max):      %lu/%lu\n", stats.min_chars_in_title, stats.max_chars_in_title);
+	printf("  num_characters:                      %lu (avg %lu/article)\n", stats.num_characters, stats.num_characters / num_titles_used);
+	printf("  num skipped chars from rare fonts:   %lu\n", stats.num_skipped_chars);
+	printf("  num unique glyphs:                   %lu\n", num_bitmaps_used);
+	printf("  output file length:                  %lu\n", dest_file_len);
+	printf("  index start/length:                  %lxh/%lu\n", index_first_page * WOM_PAGE_SIZE, last_index_off - index_first_page * WOM_PAGE_SIZE);
+	printf("  bitmaps start/length:                %lxh/%lu\n", bitmaps_first_page * WOM_PAGE_SIZE, last_bitmap_off - bitmaps_first_page * WOM_PAGE_SIZE);
+	printf("  articles start/length:               %lxh/%lu\n", articles_first_page * WOM_PAGE_SIZE, dest_file_len - articles_first_page * WOM_PAGE_SIZE);
+	printf("\n");
+
 	return 0;
 
 xout:
@@ -124,10 +360,6 @@ xout:
 	printf("X wom_creator failed.\n");
 	return 1;
 }
-
-// work/zecke-1/1/02808.work
-//                articles/0/00/hash.blib
-//                fonts/Liberation_Sans_9
 
 static int load_bitmap(font_bitmaps_t* font_bitmaps, int num_bitmaps_avail, int* num_bitmaps_used,
 		const char* work_path_base, const char* font_and_size, int font_code)
@@ -207,28 +439,13 @@ static int load_bitmap(font_bitmaps_t* font_bitmaps, int num_bitmaps_avail, int*
 	}
 	fclose(pbm_f);
 	(*num_bitmaps_used)++;
+	DP(DBG_WOM_CREATOR, (MSG_INFO, "O load_bitmap() '%s' code %d width %d height %d\n",
+		new_bitmap->font_size, new_bitmap->font_code, new_bitmap->pbm_width, new_bitmap->pbm_height));
 	return 1;
 xout_pbmf:
 	fclose(pbm_f);
 xout:
 	fprintf(stderr, "X load_bitmap() failed.\n");
-	return 0;
-}
-
-static int ftell_size(const char * file, size_t * size)
-{
-	FILE * f = fopen(file, "rb");
-	if (!f) {
-		fprintf(stderr, "X Error opening %s.\n", file);
-		return -1;
-	}
-	if (fseek(f, 0, SEEK_END)) {
-		fclose(f);
-		fprintf(stderr, "X Error seeking to end of %s.\n", file);
-		return -1;
-	}
-	*size = ftell(f);
-	fclose(f);
 	return 0;
 }
 
