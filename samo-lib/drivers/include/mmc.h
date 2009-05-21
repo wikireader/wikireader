6576a60df029d6f0d992f8ff79c1182b31598022
marek@openmoko.com <marek@openmoko.com@974c56cd-8151-0410-98c4-7bb2f43fa8c1>
2009-05-21 09:10:48 +0000

    [drivers] add forgotten header file
    git-svn-id: https://wikipediardware.googlecode.com/svn/trunk@1671 974c56cd-8151-0410-98c4-7bb2f43fa8c1

---
+++ samo-lib/drivers/include/mmc.h
@@ -0,0 +1,33 @@
+//
+// Authors:	Marek Lindner <marek@openmoko.com>
+//
+//		This program is free software; you can redistribute it and/or
+//		modify it under the terms of the GNU General Public License
+//		as published by the Free Software Foundation; either version
+//		3 of the License, or (at your option) any later version.
+//
+
+
+#if !defined(_MMC_)
+#define _MMC_
+
+#include <integer.h>
+
+
+typedef unsigned char DSTATUS;
+
+/* Results of Disk Functions */
+typedef enum {
+	RES_OK = 0,		/* 0: Successful */
+	RES_ERROR,		/* 1: R/W Error */
+	RES_WRPRT,		/* 2: Write Protected */
+	RES_NOTRDY,		/* 3: Not Ready */
+	RES_PARERR,		/* 4: Invalid Parameter */
+} DRESULT;
+
+
+DSTATUS mmc_disk_initialize(BYTE drv);
+DRESULT mmc_disk_read(BYTE drv, BYTE *buff, DWORD sector, BYTE count);
+DRESULT mmc_disk_write(BYTE drv, const BYTE *buff, DWORD sector, BYTE count);
+
+#endif
