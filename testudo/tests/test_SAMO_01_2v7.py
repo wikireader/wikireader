e84fcae84c99f0e6d997b337d281fd8edf4f414c
hsw@openmoko.com <hsw@openmoko.com@974c56cd-8151-0410-98c4-7bb2f43fa8c1>
2009-05-22 04:34:11 +0000

    [testudo] Add test that just changes the supply voltage
    Signed-off-by: Christopher Hall <hsw@openmoko.com>
    
    
    git-svn-id: https://wikipediardware.googlecode.com/svn/trunk@1680 974c56cd-8151-0410-98c4-7bb2f43fa8c1

---
+++ testudo/tests/test_SAMO_01_2v7.py
@@ -0,0 +1,14 @@
+# test main board
+# -*- coding: utf-8 -*-
+# COPYRIGHT: Openmoko Inc. 2009
+# LICENSE: GPL Version 2 or later
+# DESCRIPTION: Main board test
+# AUTHOR: Christopher Hall <hsw@openmoko.com>
+
+import os.path
+
+MASTER_TEST = 'test_SAMO_01.py'
+execfile(os.path.join(os.path.dirname(module_name), MASTER_TEST))
+
+# override MASTER_TEST settings
+SUPPLY_STANDARD_VOLTAGE = 2.7
