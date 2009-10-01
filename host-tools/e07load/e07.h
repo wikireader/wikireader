b71817723383d5858e5839f6826340fa154c6573
Christopher Hall <hsw@openmoko.com>
2009-10-01 18:14:31 +0800

    [e07load] removed
    Signed-off-by: Christopher Hall <hsw@openmoko.com>

--- host-tools/e07load/e07.h
+++
@@ -1,8 +0,0 @@
-#ifndef E07_H
-#define E07_H
-
-int sync_cpu(int fd);
-int bootstrap(int ttyfd, const char *bootstrap_file);
-
-#endif /* E07_H */
-
--- host-tools/e07load/tty.h
+++
@@ -1,7 +0,0 @@
-#ifndef TTY_H
-#define TTY_H
-
-int open_tty(const char *tty);
-
-#endif /* TTY_H */
-
