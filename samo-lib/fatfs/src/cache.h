0bcb2a5ba14d083f7b7e1b9b2ba99fb0cae6972b
Christopher Hall <hsw@openmoko.com>
2009-10-12 10:22:41 +0800

    [fatfs] removed useless cache code
    Apart from having no copyright or license
    this code suffered from writing to null pointer
    
    Signed-off-by: Christopher Hall <hsw@openmoko.com>

--- samo-lib/fatfs/src/cache.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CACHE_H
-
-DSTATUS cache_read_sector (BYTE *buff, DWORD sector);
-DSTATUS cache_write_sector (const BYTE *buff, DWORD sector);
-void cache_update_sector (const BYTE *buff, DWORD sector);
-DSTATUS cache_init (void);
-
-#endif /* _CACHE_H */
-
