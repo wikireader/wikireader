7f1a7b65d64a448f39151ffb70cad3371b339d91
Christopher Hall <hsw@openmoko.com>
2009-11-06 14:27:31 +0800

    [grifo] A simple event queue using circular buffer
    Temporary, just to make something work for CTP driver
    
    Later will be changed to a buffer that allows removal of
    specific event types.
    
    Signed-off-by: Christopher Hall <hsw@openmoko.com>

---
+++ samo-lib/grifo/src/event.c
@@ -0,0 +1,92 @@
+/*
+ * event queue
+ *
+ * Copyright (c) 2009 Christopher Hall <hsw@openmoko.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdbool.h>
+#include <inttypes.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "standard.h"
+#include "interrupt.h"
+#include "timer.h"
+#include "event.h"
+
+
+// the queue
+event_t EventQueue[256];
+
+event_t *head;
+event_t *tail;
+
+void Event_initialise(void)
+{
+	static bool initialised = false;
+	if (!initialised) {
+		initialised = true;
+		Timer_initialise();
+		Event_flush();
+	}
+}
+
+
+void Event_flush(void)
+{
+	Interrupt_type state = Interrupt_disable();
+
+	head = tail = EventQueue;
+
+	Interrupt_enable(state);
+}
+
+#include "serial.h"
+bool Event_put(const event_t *event)
+{
+	Interrupt_type state = Interrupt_disable();
+	event_t *next = &tail[1];
+
+	if (next >= &EventQueue[SizeOfArray(EventQueue)]) {
+		next = EventQueue;
+	}
+	bool full = head == next;
+	if (!full) {
+		memcpy(tail, event, sizeof(*tail));
+		tail->time_stamp = Timer_get();  // timestamp the event
+		tail = next;
+	}
+	Interrupt_enable(state);
+	return !full;  // false if buffer full
+}
+
+
+event_item_t Event_get(event_t *event)
+{
+	Interrupt_type state = Interrupt_disable();
+	if (head == tail) {
+		memset(event, 0, sizeof(*event));
+		event->item_type = EVENT_NONE;
+	} else {
+		memcpy(event, head, sizeof(*event));
+		++head;
+		if (head >= &EventQueue[SizeOfArray(EventQueue)]) {
+			head = EventQueue;
+		}
+	}
+	Interrupt_enable(state);
+	return event->item_type;
+}
---
+++ samo-lib/grifo/src/event.h
@@ -0,0 +1,82 @@
+/*
+ * event queue
+ *
+ * Copyright (c) 2009 Christopher Hall <hsw@openmoko.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#if !defined(_EVENT_H_)
+#define _EVENT_H_ 1
+
+#include <inttypes.h>
+#include <stdbool.h>
+
+
+//+MakeSystemCalls: types
+typedef enum {
+	EVENT_NONE = 0,
+	EVENT_KEY,
+	EVENT_TOUCH_DOWN,
+	EVENT_TOUCH_MOTION,
+	EVENT_TOUCH_UP,
+	EVENT_BUTTON_DOWN,
+	EVENT_BUTTON_UP,
+	EVENT_BATTERY_LOW,
+} event_item_t;
+
+typedef enum {
+	BUTTON_SEARCH,
+	BUTTON_HISTORY,
+	BUTTON_RANDOM,
+	BUTTON_POWER,
+} button_t;
+
+typedef struct event_struct {
+	event_item_t item_type;
+	unsigned long time_stamp;
+	union {
+		struct {
+			uint8_t code;
+		} key;
+
+		struct {
+			button_t code;
+		} button;
+
+		struct {
+			int x, y;
+		} touch;
+
+		struct {
+			uint32_t millivolts;
+		} battery;
+	};
+} event_t;
+//-MakeSystemCalls: types
+
+
+void Event_initialise();
+
+void Event_flush(void);
+
+// copy to buffer
+// return false if buffer is already full
+bool Event_put(const event_t *event);
+
+// copy from buffer
+// return EVENT_NONE if buffer was empty
+event_item_t Event_get(event_t *event);
+
+#endif
