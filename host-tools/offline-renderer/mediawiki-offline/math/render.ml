fe8d3d67717175f4e6aa44e64f0fca21786eab90
Sean Moss-Pultz <sean@openmoko.com>
2009-12-09 11:41:40 +0800

    [offline-renderer] force dvipng and no anti-aliasing
    Signed-off-by: Sean Moss-Pultz <sean@openmoko.com>

--- host-tools/offline-renderer/mediawiki-offline/math/render.ml
+++ host-tools/offline-renderer/mediawiki-offline/math/render.ml
@@ -5,7 +5,8 @@ let cmd_convert tmpprefix finalpath = "convert -quality 100 -density 120 " ^ tmp
 (* Putting -bg Transparent in dvipng's arguments will give full-alpha transparency *)
 (* Note that IE have problems with such PNGs and need an additional javascript snippet *)
 (* Putting -bg transparent in dvipng's arguments will give binary transparency *)
-let cmd_dvipng tmpprefix finalpath = "dvipng -gamma 1.5 -D 120 -T tight --strict " ^ tmpprefix ^ ".dvi -o " ^ finalpath ^ " >/dev/null 2>/dev/null"
+let cmd_dvipng tmpprefix finalpath = "dvipng -freetype0 -Q 1 -gamma 1.0 -D 85 -T tight --strict " ^ tmpprefix ^ ".dvi -o " ^ finalpath ^ " >/dev/null 2>/dev/null"
+let _cmd_dvipng tmpprefix finalpath = "dvipng -gamma 1.5 -D 120 -T tight --strict " ^ tmpprefix ^ ".dvi -o " ^ finalpath ^ " >/dev/null 2>/dev/null"
 
 exception ExternalCommandFailure of string
 
@@ -13,28 +14,31 @@ let render tmppath finalpath outtex md5 =
     let tmpprefix0 = (string_of_int (Unix.getpid ()))^"_"^md5 in
     let tmpprefix = (tmppath^"/"^tmpprefix0) in
     let unlink_all () =
-      begin
-        (* Commenting this block out will aid in debugging *)
-	Sys.remove (tmpprefix ^ ".dvi");
-	Sys.remove (tmpprefix ^ ".aux");
-	Sys.remove (tmpprefix ^ ".log");
-        Sys.remove (tmpprefix ^ ".tex");
-	if Sys.file_exists (tmpprefix ^ ".ps")
-	then Sys.remove (tmpprefix ^ ".ps");
-      end in
-    let f = (Util.open_out_unless_exists (tmpprefix ^ ".tex")) in
-      begin
-	output_string f (Texutil.get_preface ());
-	output_string f outtex;
-	output_string f (Texutil.get_footer ());
-	close_out f;
-	if Util.run_in_other_directory tmppath (cmd_latex tmpprefix0) != 0
-	then (unlink_all (); raise (ExternalCommandFailure "latex"))
-	else if (Sys.command (cmd_dvipng tmpprefix (finalpath^"/"^md5^".png")) != 0)
-	then (if (Sys.command (cmd_dvips tmpprefix) != 0)
+        begin
+            (* Commenting this block out will aid in debugging *)
+            Sys.remove (tmpprefix ^ ".dvi");
+            Sys.remove (tmpprefix ^ ".aux");
+            Sys.remove (tmpprefix ^ ".log");
+            Sys.remove (tmpprefix ^ ".tex");
+            if Sys.file_exists (tmpprefix ^ ".ps")
+	        then Sys.remove (tmpprefix ^ ".ps");
+        end in
+        let f = (Util.open_out_unless_exists (tmpprefix ^ ".tex")) in
+        begin
+            output_string f (Texutil.get_preface ());
+            output_string f outtex;
+            output_string f (Texutil.get_footer ());
+            close_out f;
+            if Util.run_in_other_directory tmppath (cmd_latex tmpprefix0) != 0
+            then (unlink_all (); raise (ExternalCommandFailure "latex"))
+            else if (Sys.command (cmd_dvipng tmpprefix (finalpath^"/"^md5^".png")) != 0)
+            then (unlink_all (); raise (ExternalCommandFailure "dvipng"))
+
+(*	then (if (Sys.command (cmd_dvips tmpprefix) != 0)
 	then (unlink_all (); raise (ExternalCommandFailure "dvips"))
 	else if (Sys.command (cmd_convert tmpprefix (finalpath^"/"^md5^".png")) != 0)
 	then (unlink_all (); raise (ExternalCommandFailure "convert"))
 	else unlink_all ())
 	else unlink_all ()
-      end
+*)
+        end
